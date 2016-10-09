2af530aa78dcbd1fe0a5b34d2e311b4479681ce4
searchivarius <leo@boytsov.info>
2016-10-09 01:41:43 -0400

    updating annotation script to get rid of train[12]

--- scripts/uima/run_annot_pipeline.sh
+++ scripts/uima/run_annot_pipeline.sh
@@ -17,7 +17,7 @@ if [ "$collect" = "manner" ] ; then
     fi
   done
 elif [ "$collect" = "compr" ] ; then
-  for d in Dev1 Dev2 Train1 Train2 Tran Test ; do 
+  for d in Dev1 Dev2 Train Tran Test ; do 
     echo $d 
     scripts/uima/cpe.sh src/main/resources/descriptors/collection_processing_engines/cpeAnnotCompr${d}.xml 2>&1|tee log_$d
     if [ "$?" != "0" ] ; then
