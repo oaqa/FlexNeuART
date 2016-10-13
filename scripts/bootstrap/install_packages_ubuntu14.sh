5d9b82be4c2775b6382b8e630bbd1617adf3f236
searchivarius <leo@boytsov.info>
2016-10-13 11:59:15 -0400

    Installing a full suit of software, including the patched ClearNLP.

--- scripts/bootstrap/install_packages_ubuntu14.sh
+++ scripts/bootstrap/install_packages_ubuntu14.sh
@@ -80,6 +80,7 @@ MAIN_PACKAGE_LIST="wget g++  \
                       cmake \
                       mc \
                       maven \
+                      wordnet-base \
                       python-scipy \
                       libboost-dev libboost-test-dev \
                       libboost-program-options-dev libboost-system-dev \
@@ -170,6 +171,26 @@ NMSLIB_DIR="$INSTALL_DIR/nmslib/similarity_search"
 cd "$KNN4QA_DIR" ; check "cd $KNN4QA_DIR"
 run_cmd "scripts/data/create_knn4qa_links.sh \"$DATA_DIR\""
 run_cmd "scripts/data/create_output_dirs.sh"
+run_cmd "mvn compile"
+
+echo "Links from data directory to $KNN4QA_DIR are created!"
+
+# Returning to software home dir
+run_cmd "git clone https://github.com/searchivarius/clearnlp-clearnlp-2.0.2.mod.git"
+cd clearnlp-clearnlp-2.0.2.mod ; check "cd clearnlp-clearnlp-2.0.2.mod"
+run_cmd "mvn compile"
+run_cmd "mvn install"
+
+echo "Installed a patched version of Clear NLP 2.0.2!"
+
+# Installing trec_eval
+
+run_cmd "wget https://github.com/usnistgov/trec_eval/archive/v9.0.4.tar.gz -O v9.0.4.tar.gz"
+run_cmd "tar -zxvf v9.0.4.tar.gz"
+cd trec_eval-9.0.4/ ; check "cd trec_eval-9.0.4/"
+run_cmd "make -j $BUILD_THREAD_QTY"
+
+echo "trec_eval is installed!"
 
 # Returning to data home dir
 cd "$DATA_DIR" ; check "cd $DATA_DIR"
