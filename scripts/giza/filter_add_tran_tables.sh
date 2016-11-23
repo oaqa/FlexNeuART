ad168d5d72e341344b1a417ded114c352aa4654d
searchivarius <leo@boytsov.info>
2016-11-23 15:03:44 -0500

    minor fix

--- scripts/giza/filter_add_tran_tables.sh
+++ scripts/giza/filter_add_tran_tables.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+function check {
+  f="$?"
+  name=$1
+  if [ "$f" != "0" ] ; then
+    echo "**************************************"
+    echo "* Failed: $name"
+    echo "**************************************"
+    exit 1
+  fi
+}
+
+FIELD="text"
+ITER_QTY="5"
 MIN_PROB="2.5e-3"
 
 COL=$1
@@ -13,5 +26,7 @@ for d in  tran/$COL/tran*
 do 
   if [ -d $d ] ; then
     echo "Processing folder $d with minimum translation probability: $MIN_PROB"
+    scripts/giza/simple_filter_tran_table.sh "$d" $FIELD $ITER_QTY $MIN_PROB
+    check "scripts/giza/simple_filter_tran_table.sh "$d" $FIELD $ITER_QTY $MIN_PROB"
   fi
 done
