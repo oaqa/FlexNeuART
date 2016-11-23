9387289a54bc914d4ef5559cf1474ffa79689c05
searchivarius <leo@boytsov.info>
2016-11-23 15:25:54 -0500

    skeleton script to run additional translation experiment

---
+++ scripts/exper/add_tran_tables_exper.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function check {
+  f="$?"
+  name=$1
+  if [ "$f" != "0" ] ; then
+    echo "**************************************"
+    echo "* Failed: $name"
+    echo "**************************************"
+    exit 1
+  fi
+}
+
+COL=$1
+
+if [ "$COL" = "" ] ; then
+  print "Specify the collection (e.g., compr, stackoverflow) as the 1st arg"
+  exit 1
+fi
+
+FIELD="text"
+DPREF="tran/$COL"
+for n in `ls  $DPREF/|grep tran` ; do
+do 
+  d=$DPREF/$n
+  if [ -d $d ] ; then
+    echo "Processing folder $d"
+    if [ -h "$DPREF/$FIELD" ] ; then
+      rm $DPREF/$FIELD
+      check "rm $DPREF/$FIELD"
+    fi
+    cd $DPREF
+    check "cd $DPREF"
+    ln -s $d/$FIELD  
+    check "ln -s $d/$FIELD"
+  fi
+done
