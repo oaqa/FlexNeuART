d3ed4c0e3337bfbc4776829c34cc5af95d0c0944
searchivarius <leo@boytsov.info>
2016-11-28 16:13:31 -0500

    a bit of improvement for trectext conversion

--- scripts/data/convert_to_trectext.sh
+++ scripts/data/convert_to_trectext.sh
@@ -5,16 +5,27 @@ if [ "$collect" = "" ] ; then
   exit 1
 fi
 
-out_file=$2
-if [ "$out_file" = "" ] ; then
-  echo "Specify the output file (2d arg)"
-  exit 1
-fi
-
-
 INPUT=""
 for part in dev1 dev2 test train tran ; do
   INPUT="$INPUT -input_file output/$collect/$part/SolrAnswerFile.txt"
 done
 
-scripts/data/run_convert_to_trectext.sh $INPUT -output_file "$out_file"
+function check {
+  f="$?"
+  name=$1
+  if [ "$f" != "0" ] ; then
+    echo "**************************************"
+    echo "* Failed: $name"
+    echo "**************************************"
+    exit 1
+  fi
+}
+
+OUTPUT_DIR="output_trectext/$collect/"
+if [ ! -d $OUTPUT_DIR ] ; 
+  mkdir -p "$OUTPUT_DIR"
+  check "mkdir -p $OUTPUT_DIR"
+fi
+
+scripts/data/run_convert_to_trectext.sh $INPUT -output_file "$OUTPUT_DIR/answers.txt"
+check "scripts/data/run_convert_to_trectext.sh $INPUT -output_file $OUTPUT_DIR/answers.txt"
