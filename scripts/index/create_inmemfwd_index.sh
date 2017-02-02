85e072d5e723dc30fb5d64784b8ce36ef08fbc11
searchivarius <leo@boytsov.info>
2017-02-01 20:51:57 -0500

    fix

--- scripts/index/create_inmemfwd_index.sh
+++ scripts/index/create_inmemfwd_index.sh
@@ -63,7 +63,7 @@ elif [ "$collect" = "squad" ] ; then
     check "scripts/index/run_inmemfwd_index.sh $store_word_id_seq_param -root_dir $IN_DIR  -index_dir $OUT_DIR -sub_dirs train,dev1,dev2,test,tran -solr_file $JOINT_NAME -field $field"
   done
 elif [ "$collect" = "gov2" ] ; then
-  scripts/index/run_inmemfwd_index.sh -root_dir $IN_DIR  -index_dir $OUT_DIR -sub_dirs all -solr_file SolrAnswerFile.txt.gz
+  scripts/index/run_inmemfwd_index.sh -field text -root_dir $IN_DIR  -index_dir $OUT_DIR -sub_dirs all -solr_file SolrAnswerFile.txt.gz
   if [ "$?" != "0" ] ; then
     echo "FAILURE!!!"
     exit 1
