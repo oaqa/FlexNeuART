995b962c5c3c1c571e2d17edb74da45f0700ccb0
searchivairus <leo@boytsov.info>
2017-03-09 00:30:43 -0500

    Mostly adding new headers for manner.

--- scripts/index/create_inmemfwd_index.sh
+++ scripts/index/create_inmemfwd_index.sh
@@ -33,7 +33,8 @@ rm -f "$OUT_DIR"/*
 echo "=========================================================================="
 
 if [ "$collect" = "manner" ] ; then
-  for field in text text_unlemm bigram srl srl_lab dep wnss ; do
+  #for field in text text_unlemm bigram srl srl_lab dep wnss ; do
+  for field in text ; do
     scripts/index/run_inmemfwd_index.sh $store_word_id_seq_param -root_dir $IN_DIR  -index_dir $OUT_DIR -sub_dirs train,dev1,dev2,test -solr_file SolrAnswerFile.txt -field $field
     check "scripts/index/run_inmemfwd_index.sh  $store_word_id_seq_param -root_dir $IN_DIR  -index_dir $OUT_DIR -sub_dirs train,dev1,dev2,test -solr_file SolrAnswerFile.txt -field $field"
   done
