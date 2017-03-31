7cf68ffb7a93edde2378644dfc9918083a271f1d
searchivairus <leo@boytsov.info>
2017-03-31 00:04:48 -0400

    new script to obtain collection data.

---
+++ scripts/data/get_collect_pardata_stat.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+. scripts/common.sh
+
+collect=$1
+if [ "$collect" = "" ] ; then
+  echo "Specify a collection: manner, compr, stackoverflow, squad (1st arg)"
+  exit 1
+fi
+
+wcq=(`wc output/$collect/tran/question_text`)
+wca=(`wc output/$collect/tran/answer_text`)
+echo -e "# of questions:\t${wcq[0]}"
+echo -e "# of answers:\t${wca[0]}"
+qt=`awk "BEGIN{printf(\"%.1f\", ${wcq[1]}/${wcq[0]})}"`
+echo -e "# of terms per question:\t$qt"
+at=`awk "BEGIN{printf(\"%.1f\", ${wca[1]}/${wca[0]})}"`
+echo -e "# of terms per answer:\t$qt"
+
