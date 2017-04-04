f367b11ccda56946172914f9557a09582b8ad3da
searchivairus <leo@boytsov.info>
2017-04-03 20:46:05 -0400

    A simple script to backup results.

---
+++ scripts/data/backup_results_and_logs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+. scripts/common.sh
+
+if [ "$KNN4QA_COLLECT" = "" ] ; then
+  echo "Error environment variable KNN4QA_COLLECT is not set, should be compr,stackoverflow,squad, ..."
+  exit 1
+fi
+
+cp logs.* ~/data/logs/$KNN4QA_COLLECT
+check "cp logs.* ~/data/logs/$KNN4QA_COLLECT"
+
+cp server.logs.* ~/data/logs/$KNN4QA_COLLECT
+check "cp server.logs.* ~/data/logs/$KNN4QA_COLLECT"
+
+dt=`date +%Y-%m-%d` 
+check "dt ..."
+
+tar cvfj results_${KNN4QA_COLLECT}_${dt}.bz2 results*
+check "tar cvfj results_${KNN4QA_COLLECT}_${dt}.bz2 results*"
+
+tar cvfj logs_${KNN4QA_COLLECT}_${dt}.bz2 ~/data/logs/$KNN4QA_COLLECT/*"
+check "tar cvfj results_${KNN4QA_COLLECT}_${dt}.bz2 ~/data/logs/$KNN4QA_COLLECT*"
