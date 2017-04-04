3f33c4591c42067b5ef0521834517cb3adafc7c4
searchivairus <leo@boytsov.info>
2017-04-03 21:31:00 -0400

    ...

--- scripts/data/backup_results_and_logs.sh
+++ scripts/data/backup_results_and_logs.sh
@@ -6,11 +6,22 @@ if [ "$KNN4QA_COLLECT" = "" ] ; then
   exit 1
 fi
 
-cp log.* ~/data/logs/$KNN4QA_COLLECT
-check "cp log.* ~/data/logs/$KNN4QA_COLLECT"
+DATA_DIR=$1
 
-cp server.log.* ~/data/logs/$KNN4QA_COLLECT
-check "cp server.logs.* ~/data/logs/$KNN4QA_COLLECT"
+if [ "$DATA_DIR" = "" ] ; then
+  echo "Enter the name of the data directory!"
+  exit 1
+fi
+if [ ! -d "$DATA_DIR" ] ; then
+  echo "$DATA_DIR is not a directory!"
+  exit 1
+fi
+
+cp log.* $DATA_DIR/logs/$KNN4QA_COLLECT
+check "cp log.* $DATA_DIR/logs/$KNN4QA_COLLECT"
+
+cp server.log.* $DATA_DIR/logs/$KNN4QA_COLLECT
+check "cp server.logs.* $DATA_DIR/logs/$KNN4QA_COLLECT"
 
 dt=`date +%Y-%m-%d` 
 check "dt ..."
@@ -18,8 +29,8 @@ check "dt ..."
 tar cvfj results_${KNN4QA_COLLECT}_${dt}.bz2 results*
 check "tar cvfj results_${KNN4QA_COLLECT}_${dt}.bz2 results*"
 
-tar cvfj logs_${KNN4QA_COLLECT}_${dt}.bz2 ~/data/logs/$KNN4QA_COLLECT/*
-check "tar cvfj logs_${KNN4QA_COLLECT}_${dt}.bz2 ~/data/logs/$KNN4QA_COLLECT*"
+tar cvfj logs_${KNN4QA_COLLECT}_${dt}.bz2 $DATA_DIR/logs/$KNN4QA_COLLECT/*
+check "tar cvfj logs_${KNN4QA_COLLECT}_${dt}.bz2 $DATA_DIR/logs/$KNN4QA_COLLECT*"
 
 tar cvfj gs_cache_${KNN4QA_COLLECT}_${dt}.bz2 gs_cache/*
 check "tar cvfj gs_cache_${KNN4QA_COLLECT}_${dt}.bz2 gs_cache/*"
