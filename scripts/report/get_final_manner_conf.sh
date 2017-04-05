d07b15575467755aabc573e716280fc54a0a9ae1
searchivairus <leo@boytsov.info>
2017-04-05 17:22:33 -0400

    computing conf intervals for recall

--- scripts/report/get_final_manner_conf.sh
+++ scripts/report/get_final_manner_conf.sh
@@ -42,7 +42,7 @@ if [ "$SKIP_RUN_GEN" != "1" ] ; then
         echo "Missing file $frep"
       fi
       TREC_EVAL_FNAME="$REPORT_DIR/trec_eval_mod.$N"
-      # Valuse for non-zero recall are obtained by retaining recors with non-zero reciprocal rank (RR) only
+      # Values for non-zero recall are obtained by retaining recors with non-zero reciprocal rank (RR) only
       bzcat $frep|awk '{print $1" "$2" "$3}'|grep -E '^recip_rank '|grep -Ev '(all|recip_rank .* 0\.00*$)'|awk '{vp1=0;if ($3>=1) vp1=1; print "recip_rank\t"$2"\t"$3; print "P_1\t"$2"\t"vp1;}' > $TREC_EVAL_FNAME
       REGISTRY_FILE="$REPORT_DIR/registry.$N"
       echo $TREC_EVAL_FNAME > $REGISTRY_FILE
@@ -51,12 +51,20 @@ if [ "$SKIP_RUN_GEN" != "1" ] ; then
         scripts/report/conv_treceval.pl $metric $REGISTRY_FILE $RUN_ROW.$metric
         check "scripts/report/conv_treceval.pl $metric $REGISTRY_FILE $RUN_ROW.$metric"
       done
+
+      TREC_EVAL_FNAME_ADD="$REPORT_DIR/trec_eval_mod_add.$N"
+      echo $TREC_EVAL_FNAME_ADD > $REGISTRY_FILE
+      bzcat $frep|awk '{print $1" "$2" "$3}'|grep -E '^num_rel_ret '|grep -v 'all' > $TREC_EVAL_FNAME_ADD
+      for metric in num_rel_ret ; do
+        scripts/report/conv_treceval.pl $metric $REGISTRY_FILE $RUN_ROW.$metric
+        check "scripts/report/conv_treceval.pl $metric $REGISTRY_FILE $RUN_ROW.$metric"
+      done
     done
 else
   echo "Skipping run-data generation!"
 fi
 
-for metric in P_1 recip_rank ; do
+for metric in num_rel_ret P_1 recip_rank ; do
   echo "Metric $metric"
   echo "========================================"
   for N in 10 17 36 72
