784b66578c5b64f909e928c330784b0232345f0e
searchivairus <leo@boytsov.info>
2017-04-05 16:15:57 -0400

    Cosmetic improvements for conf. intervals

--- scripts/report/conf-interv.R
+++ scripts/report/conf-interv.R
@@ -11,7 +11,8 @@ digit  <- as.numeric(args[3])
 dt <- as.numeric(read.table(run   ))
 
 m <- mean(dt)
-d <- qnorm((1-pct/200.0))
+print(paste("pct=", pct, sep=""))
+d <- qnorm((1-(100-pct)/200.0))
 print(paste("sigma coeff=",d, sep=" "))
 n <- length(dt)
 s <- sd(dt)
@@ -19,7 +20,7 @@ s_norm <- s/sqrt(n)
 print(run)
 print(paste("sd=", s, " normalized sd=", s_norm, sep=""))
 print(paste("[ ", round(m - d*s_norm,digit), ",", round(m, digit), ",", round(m + d*s_norm,digit), " ]", sep=""))
-print(paste(round(m, digit), '$\\pm$', round(d*s_norm,digit), sep=""))
+print(paste(round(m, digit), "$\\pm$", round(d*s_norm,digit), sep=""))
 
 print("----------------------------")
 
