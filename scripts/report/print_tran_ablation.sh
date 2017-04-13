0ea1985b4f7bd9248d7e23464c06ef12b09501bf
searchivairus <leo@boytsov.info>
2017-04-13 00:08:15 -0400

    a simple helper script to proc results

---
+++ scripts/report/print_tran_ablation.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+. scripts/common.sh
+
+col=$1
+if [ "$col" = "" ] ; then
+  echo "Specify collection name, e.g., squad (1st arg)"
+  exit 1
+fi
+
+function one_set {
+  dir=$1
+  ndcg=`cat $1/@/rep/out_100.rep|grep -i NDCG|awk '{print $2}'`
+  err=`cat $1/@/rep/out_100.rep|grep -i ERR|awk '{print $2}'`
+  echo -e "$ndcg\t$err"
+}
+
+one_set results.tune/feature_exper/$col/qrels_all_graded_same_score.txt/dev1//exper@bm25\=text+model1=text+minProbModel1\=text\:2.5e-3/
+
+for f in 2 4 8 16 32 64 128 250 ; do  
+  one_set results.tran$f/feature_exper/compr/qrels_all_graded_same_score.txt/dev1/exper@bm25\=text+model1=text/
+done
