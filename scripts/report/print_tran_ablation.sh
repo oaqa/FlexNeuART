611dd0387c40dcc212912c103e1f51557ab5dd62
searchivairus <leo@boytsov.info>
2017-04-13 01:38:43 -0400

    ablation info extr. for SQuAD

--- scripts/report/print_tran_ablation.sh
+++ scripts/report/print_tran_ablation.sh
@@ -17,8 +17,18 @@ function one_set {
 echo "BM25:"
 one_set results.tune/feature_exper/$col/qrels_all_graded_same_score.txt/dev1//exper@bm25\=text/
 
-echo "BM25+Model1:"
-one_set results.tune/feature_exper/$col/qrels_all_graded_same_score.txt/dev1//exper@bm25\=text+model1=text+minProbModel1\=text\:2.5e-3/
-for f in 2 4 8 16 32 64 128 256 ; do  
-  one_set results.tran$f/feature_exper/$col/qrels_all_graded_same_score.txt/dev1/exper@bm25\=text+model1=text/
-done
+if [ "$col" != "squad" ] ; then
+
+  echo "BM25+Model1:"
+  one_set results.tune/feature_exper/$col/qrels_all_graded_same_score.txt/dev1//exper@bm25\=text+model1=text+minProbModel1\=text\:2.5e-3/
+  for f in 2 4 8 16 32 64 128 256 ; do  
+    one_set results.tran$f/feature_exper/$col/qrels_all_graded_same_score.txt/dev1/exper@bm25\=text+model1=text/
+  done
+else
+  echo "BM25+Model1 (SQuAD):"
+  one_set results/feature_exper/squad/qrels_all_graded.txt/dev1/exper@bm25\=text+model1\=text/
+  echo "BM25+Model1 (Compr):"
+  one_set results/feature_exper/squad/qrels_all_graded.txt/dev1/exper@bm25\=text+model1\=text_alias1/
+  echo "BM25+Model1 (SQuAD+Compr):"
+  one_set results/feature_exper/squad/qrels_all_graded.txt/dev1/exper@bm25\=text+model1\=text+model\=text_alias1/
+fi
