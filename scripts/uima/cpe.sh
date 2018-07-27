a5664b294848ff01718f46854e0c64464d15c92d
Leonid Boytsov <leo@Leonids-MacBook-Pro.local>
2018-07-27 13:31:25 -0400

    increasing memory size.

--- scripts/uima/cpe.sh
+++ scripts/uima/cpe.sh
@@ -12,12 +12,15 @@ OS=`uname|awk '{print $1}'`
 if [ "$OS" = "Linux" ] ; then
   MEM_SIZE_MX_KB=`free|grep Mem|awk '{print $2}'`
   MEM_SIZE_MIN_KB=$((4*$MEM_SIZE_MX_KB/5))
-  export MAVEN_OPTS="-Xms${MEM_SIZE_MIN_KB}k -Xmx${MEM_SIZE_MX_KB}k -server"
+  #export MAVEN_OPTS="-Xms${MEM_SIZE_MIN_KB}k -Xmx${MEM_SIZE_MX_KB}k -server"
+  # No mx
+  export MAVEN_OPTS="-Xms${MEM_SIZE_MIN_KB}k -server"
 elif [ "$OS" = "Darwin" ] ; then
   # Assuming Macbook pro
   MEM_SIZE_MX_KB=$((16384*1024))
   MEM_SIZE_MIN_KB=$((3*$MEM_SIZE_MX_KB/4))
-  export MAVEN_OPTS="-Xms${MEM_SIZE_MIN_KB}k -Xmx${MEM_SIZE_MX_KB}k -server"
+  #export MAVEN_OPTS="-Xms${MEM_SIZE_MIN_KB}k -Xmx${MEM_SIZE_MX_KB}k -server"
+  export MAVEN_OPTS="-Xms${MEM_SIZE_MIN_KB}k -server"
 fi
 mvn compile exec:java -Dexec.mainClass=edu.cmu.lti.oaqa.knn4qa.apps.SimpleRunCPE_fixed  -Dexec.args="$DescPath"
 if [ "$?" != "0" ] ; then
