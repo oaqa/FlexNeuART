cf9369ffbc9a264d9d09fa90690cd32be8c0eb30
searchivarius <leo@boytsov.info>
2018-12-29 00:34:32 -0500

    decreasing memory requirements for indexing utils.

--- scripts/index/run_lucene_index.sh
+++ scripts/index/run_lucene_index.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 source scripts/common.sh
-setJavaMem 3 4
+setJavaMem 1 4
 bash_cmd="mvn compile exec:java -Dexec.mainClass=edu.cmu.lti.oaqa.knn4qa.apps.LuceneIndexer -Dexec.args='$@' "
 bash -c "$bash_cmd"
 if [ "$?" != "0" ] ; then
