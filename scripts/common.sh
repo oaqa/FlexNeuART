8408c578462b921f17417b36486fd46dfd903168
searchivarius <leo@boytsov.info>
2019-01-11 19:00:50 -0500

    ...

--- scripts/common.sh
+++ scripts/common.sh
@@ -86,6 +86,7 @@ function setJavaMem {
   fi
   # No mx
   MEM_SIZE_MIN_KB=$(($F1*$MEM_SIZE_MX_KB/$F2))
-  export MAVEN_OPTS="-Xms${MEM_SIZE_MIN_KB}k -Xmx${MEM_SIZE_MX_KB} -server"
+  MEM_SIZE_MAX_KB=$((5*$MEM_SIZE_MX_KB/6))
+  export MAVEN_OPTS="-Xms${MEM_SIZE_MIN_KB}k -Xmx${MEM_SIZE_MAX_KB} -server"
   echo "MAVEN_OPTS=$MAVEN_OPTS"
 }
