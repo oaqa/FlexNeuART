576c4674ff184bd3f67ae37a13242de66decd3f0
searchivarius <leo@boytsov.info>
2019-01-23 23:55:00 -0500

    ...

--- scripts/num_ret_list.sh
+++ scripts/num_ret_list.sh
@@ -2,4 +2,4 @@
 # If needed we can make a collection-specific choice
 export N_TRAIN=20 # for NDCG@20 and ERR@20
 #export NUM_RET_LIST="10,25,50,75,100,250,500,1000"
-export NUM_RET_LIST="10,50,100,250"
+export NUM_RET_LIST="10,50,100,250,500,1000"
