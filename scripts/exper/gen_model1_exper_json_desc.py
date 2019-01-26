376448b9e8254e4e0335d7fefcb6ead9f145d4e7
Leonid Boytsov <leo@Leonids-MacBook-Pro.local>
2019-01-26 13:54:23 -0500

    tuning

--- scripts/exper/gen_model1_exper_json_desc.py
+++ scripts/exper/gen_model1_exper_json_desc.py
@@ -37,27 +37,22 @@ with open(os.path.join(outDescDir, '%s.desc' % confSubDir), 'w') as of:
 
   paramArr = []
 
-  #for probSelfTran in [0.05, 0.1, 0.15, 0.25, 0.3, 0.35, 0.45, 0.5]:
-  #  for lamb in [0.05, 0.1, 0.15, 0.25, 0.3, 0.35, 0.45, 0.5]:
-  #    paramArr.append( (probSelfTran, lamb, 2.5e-3))
+  for probSelfTran in [0.05, 0.1, 0.15, 0.25, 0.3, 0.35, 0.45, 0.5]:
+    for lamb in [0.05, 0.1, 0.15, 0.25, 0.3, 0.35, 0.45, 0.5]:
+      paramArr.append( (probSelfTran, lamb))
 
-  # paramArr.append( (0.6, 0.05, 2.5e-3) )
-  # paramArr.append( (0.7, 0.05, 2.5e-3) )
-  # paramArr.append( (0.8, 0.05, 2.5e-3) )
-  # paramArr.append( (0.9, 0.05, 2.5e-3) )
-  #
-  # paramArr.append((0.9, 0.01, 2.5e-3))
-  # paramArr.append((0.9, 0.001, 2.5e-3))
-  # paramArr.append((0.9, 0.0, 2.5e-3))
+  paramArr.append( (0.6, 0.05) )
+  paramArr.append( (0.7, 0.05) )
+  paramArr.append( (0.8, 0.05) )
+  paramArr.append( (0.9, 0.05) )
 
-  paramArr.append((0.9, 0.0, 1e-3))
-  paramArr.append((0.9, 0.0, 5e-4))
-  paramArr.append((0.9, 0.0, 2.5e-4))
-  paramArr.append((0.9, 0.0, 1e-4))
+  paramArr.append((0.9, 0.01))
+  paramArr.append((0.9, 0.001))
+  paramArr.append((0.9, 0.0))
 
-  for probSelfTran, lamb, minModel1Prob in paramArr:
+  for probSelfTran, lamb in paramArr:
 
-      fid = 'bm25=text+model1=text_unlemm+lambda=%g+probSelfTran=%g+minTranProb=%g' % (lamb, probSelfTran, minModel1Prob)
+      fid = 'bm25=text+model1=text_unlemm+lambda=%g+probSelfTran=%g' % (lamb, probSelfTran)
 
       print(fid)
       extrList = [{
@@ -65,9 +60,9 @@ with open(os.path.join(outDescDir, '%s.desc' % confSubDir), 'w') as of:
                     "params": {
                       "fieldName": "text_unlemm",
                       "gizaIterQty": "5",
-                      "probSelfTran": str(probSelfTran) + "f", # for float in Java
+                      "probSelfTran": probSelfTran,
                       "lambda": lamb,
-                      "minModel1Prob": minModel1Prob
+                      "minModel1Prob": "2.5e-3f"
                     }
                   }
                   ]
@@ -86,6 +81,47 @@ with open(os.path.join(outDescDir, '%s.desc' % confSubDir), 'w') as of:
       jsonFileName = fid + '.json'
       jsonPath = os.path.join(outJsonDir, jsonFileName)
       of.write('%s dev2 %s\n' % (jsonPath, os.path.join(confSubDir, fid)))
+
+
+      paramArr = []
+      paramArr.append((0.9, 0.0, 1e-3))
+      paramArr.append((0.9, 0.0, 5e-4))
+      paramArr.append((0.9, 0.0, 2.5e-4))
+      paramArr.append((0.9, 0.0, 1e-4))
+
+      for probSelfTran, lamb, minModel1Prob in paramArr:
+        fid = 'bm25=text+model1=text_unlemm+lambda=%g+probSelfTran=%g+minTranProb=%g' % (
+        lamb, probSelfTran, minModel1Prob)
+
+        print(fid)
+        extrList = [{
+          "type": "Model1Similarity",
+          "params": {
+            "fieldName": "text_unlemm",
+            "gizaIterQty": "5",
+            "probSelfTran": str(probSelfTran) + "f",  # for float in Java
+            "lambda": lamb,
+            "minModel1Prob": minModel1Prob
+          }
+        }
+        ]
+
+        extrList.append({
+          "type": "TFIDFSimilarity",
+          "params": {
+            "fieldName": "text",
+            "similType": "bm25",
+            "k1": bestBM25[colName]['k1'],
+            "b": bestBM25[colName]['b']
+          }
+        })
+
+        jsonDesc = {"extractors": extrList}
+        jsonFileName = fid + '.json'
+        jsonPath = os.path.join(outJsonDir, jsonFileName)
+        of.write('%s dev2 %s\n' % (jsonPath, os.path.join(confSubDir, fid)))
+
+
 
       with open(jsonPath, 'w') as f:
         json.dump(jsonDesc, f)
