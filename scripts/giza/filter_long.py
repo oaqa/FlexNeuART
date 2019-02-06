67c7be87dea5bf66adb6557d9201a6189c4ea554
searchivarius <leo@boytsov.info>
2019-02-06 18:42:19 -0500

    tiny fix

--- scripts/giza/filter_long.py
+++ scripts/giza/filter_long.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import sys
 if len(sys.argv) != 7:
-  print "Usage: <questions> <answers> <maximum fertility> <output questions> <output answers> <symmetrize? 1 or 0>"
+  print("Usage: <questions> <answers> <maximum fertility> <output questions> <output answers> <symmetrize? 1 or 0>")
 questFile=open(sys.argv[1], 'r')
 answFile=open(sys.argv[2], 'r')
 maxFert = int(sys.argv[3])
 outFileQuest=open(sys.argv[4], "w")
 outFileAnsw=open(sys.argv[5], "w")
 symmetr=int(sys.argv[6]) != 0
-print "Symmetrizing %d, maxFet %d" % (symmetr, maxFert)
+print("Symmetrizing %d, maxFet %d" % (symmetr, maxFert))
 for quest in questFile:
   answ = answFile.readline()
   len1=len(answ.split())
