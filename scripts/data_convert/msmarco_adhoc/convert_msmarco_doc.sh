61e285d39f4ba9696a5f299ed36bab98f2a092f0
searchivarius <leo@boytsov.info>
2019-08-03 16:59:24 -0400

    data processing updates

--- scripts/data_convert/msmarco_adhoc/convert_msmarco_doc.sh
+++ scripts/data_convert/msmarco_adhoc/convert_msmarco_doc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# The main script to convert MSMARCO collection
+# The main script to convert MSMARCO document collection
 source scripts/common_proc.sh
 src=$1
 if [ "$src" = "" ] ; then
@@ -12,7 +12,7 @@ if [ "$dst" = "" ] ; then
   exit 1
 fi
 
-for part in docs train dev test ; do
+for part in docs train dev test2019 ; do
   mkdir -p $dst/input_data/$part
 done
 
@@ -20,6 +20,8 @@ for part in train dev ; do
   zcat $src/msmarco-doc${part}-qrels.tsv.gz > $dst/input_data/$part/qrels.txt
   scripts/data_convert/msmarco_adhoc/convert_queries.py --input  $src/msmarco-doc${part}-queries.tsv.gz --output $dst/input_data/$part/QuestionFields.jsonl
 done
+
+scripts/data_convert/msmarco_adhoc/convert_queries.py --input  $src/msmarco-test2019-queries.tsv.gz --output $dst/input_data/test2019/QuestionFields.jsonl
 
 python -u scripts/data_convert/msmarco_adhoc/convert_docs.py --input $src/msmarco-docs.tsv.gz  --output $dst/input_data/docs/AnswerFields.jsonl.gz 
 
---
+++ scripts/data_convert/msmarco_adhoc/convert_msmarco_pass.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+# The main script to convert MSMARCO passage collection 
+# It should be invoked after a bit of data massaging
+# After downloading data from https://microsoft.github.io/TREC-2019-Deep-Learning/
+# 1. documents needs to recompressed to collection.tsv.gz
+# 2. queries need to be decompressed
+source scripts/common_proc.sh
+src=$1
+if [ "$src" = "" ] ; then
+  echo "Specify the source directory (1st arg)"
+  exit 1
+fi
+dst=$2
+if [ "$dst" = "" ] ; then
+  echo "Specify the target directory (2d arg)"
+  exit 1
+fi
+
+for part in pass train dev eval ; do
+  mkdir -p $dst/input_data/$part
+done
+
+for part in train dev ; do
+  cp $src/qrels.$part.tsv $dst/input_data/$part/qrels.txt
+done
+for part in train dev eval ; do
+  python -u scripts/data_convert/msmarco_adhoc/convert_queries.py --input  $src/queries.$part.tsv --output $dst/input_data/$part/QuestionFields.jsonl
+done
+
+python -u scripts/data_convert/msmarco_adhoc/convert_pass.py --input $src/collection.tsv.gz  --output $dst/input_data/pass/AnswerFields.jsonl.gz 
+
