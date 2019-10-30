0e45e8016f9bd46ca3c8df53c756eae7b2a090cf
searchivarius <leo@boytsov.info>
2019-10-30 00:37:53 -0400

    more memory to the indexing script.

--- scripts/index/run_buildfwd_index.sh
+++ scripts/index/run_buildfwd_index.sh
@@ -1,5 +1,5 @@
 #/bin/bash -e
 source scripts/common_proc.sh
-setJavaMem 4 8
+setJavaMem 6 8
 cmd="mvn compile exec:java -Dexec.mainClass=edu.cmu.lti.oaqa.knn4qa.apps.BuildFwdIndexApp -Dexec.args='$@' "
 execAndCheck "$cmd"
