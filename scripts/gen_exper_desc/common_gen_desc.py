764b36253ee5522f9fb2a3575677141e544f15c4
searchivarius <leo@boytsov.info>
2020-01-09 02:04:23 -0500

    major refactoring for parameter-generating scripts

--- scripts/gen_exper_desc/common_gen_desc.py
+++ scripts/gen_exper_desc/common_gen_desc.py
@@ -1,33 +1,58 @@
+import os
 import sys
 import argparse
-from collections import namedtuple
+import json
 
 TEST_SET_PARAM="testSet"
 EXPER_SUBDIR_PARAM="experSubdir"
 EXTR_TYPE_PARAM="extrType"
 
-ExperDescGenParams=namedtuple('ExperDescGenParams', ['outputPath', 'relDescPath'])
+class BaseParser:
+  def initAddArgs(self):
+    pass
 
-def parseArgs(progName):
-  """This function parsers arguments for experiment-generating scripts.
-  It is used to obtain the output path as well as the relative descriptor path.
-  These can be different, b/c, e.g., output path can be a full path. Relative path
-  is a path relative to the collection root.
+  def __init__(self, progName):
+    self.parser = argparse.ArgumentParser(description=progName)
+    self.parser.add_argument('--outdir', metavar='output directory',
+                        help='output directory',
+                        type=str, required=True)
+    self.parser.add_argument('--rel_desc_path', metavar='relative descriptor path',
+                        help='relative descriptor path',
+                        type=str, required=True)
+    self.initAddArgs()
 
-  :param  progName    name of the program
-  :return a named tuple with parameters
+    self.args = self.parser.parse_args()
+    print(self.args)
+
+
+def genDescriptors(args, extrJsonGenFunc, jsonDescName, jsonSubDir):
+  """
+  A generic function to write a bunch of experimental descrptors.
+
+  :param args:              arguments previously produce by the class inherited from BaseParser
+  :param extrJsonGenFunc:   generator of extractor JSON and its file ID.
+  :param jsonDescName:      the name of the top-level descriptor file that reference individual extractor JSONs.
+  :param jsonSubDir:        a sub-directory to store extractor JSONs.
 
   """
+  descDataJSON = []
 
-  parser = argparse.ArgumentParser(description='Convert MSMARCO-adhoc documents.')
-  parser.add_argument('--outdir', metavar='output directory',
-                      help='output directory',
-                      type=str, required=True)
-  parser.add_argument('--rel_desc_path', metavar='relative descriptor path',
-                      help='relative descriptor path',
-                      type=str, required=True)
+  outJsonSubDir = os.path.join(args.outdir, jsonSubDir)
+  if not os.path.exists(outJsonSubDir):
+    os.makedirs(outJsonSubDir)
 
-  args = parser.parse_args()
-  print(args)
+  for fileId, jsonDesc in extrJsonGenFunc():
+    jsonFileName = fileId + '.json'
 
-  return ExperDescGenParams(outputPath=args.outdir, relDescPath=args.rel_desc_path)
+    descDataJSON.append({EXPER_SUBDIR_PARAM: os.path.join(jsonSubDir, fileId),
+                         EXTR_TYPE_PARAM: os.path.join(args.rel_desc_path, jsonSubDir, jsonFileName)})
+
+    with open(os.path.join(outJsonSubDir, jsonFileName), 'w') as of:
+      json.dump(jsonDesc, of, indent=2)
+
+  with open(os.path.join(args.outdir, jsonDescName), 'w') as of:
+    json.dump(descDataJSON, of, indent=2)
+
+
+
+
--- scripts/gen_exper_desc/gen_bm25_tune_json_desc.py
+++ scripts/gen_exper_desc/gen_bm25_tune_json_desc.py
@@ -3,60 +3,33 @@ import os, sys, json
 sys.path.append('scripts')
 from gen_exper_desc.common_gen_desc import *
 
-# Relative output location for descriptor files:
+def extrJsonGen():
+  for bi in range(7):
+    for k1i in range(7):
+      b = 0.3 + 0.15 * bi
+      k1 = 0.4 + 0.2 * k1i
+      bstr = '%g' % b
+      k1str = '%g' % k1
+      fid = 'bm25_k1=%s_b=%s' % (k1str, bstr)
 
-# <relative descriptor path> / bm25tune / <experiment-specific suffix>.json
-# <relative descriptor path> / bm25tune.json
-
-params =
-
-outRootDir = sys.argv[1]
-outDir = os.path.join(outRootDir, 'bm25tune')
-relDescDir = sys.argv[2]
-
-if not os.path.exists(outRootDir):
-  print('Directory does not exist: %s' % outRootDir)
-  sys.exit(1)
-
-if not os.path.exists(outDir):
-  print('Directory does not exist: %s' % outDir)
-  sys.exit(1)
-
-outDescDir = outRootDir
-
-descDataJSON = []
-
-for bi in range(7):
-  for k1i in range(7):
-    b = 0.3 + 0.15 * bi
-    k1 = 0.4 + 0.2 * k1i
-    bstr = '%g' % b
-    k1str = '%g' % k1
-    fid = 'bm25_k1=%s_b=%s' % (k1str, bstr)
-    print(fid)
-    jsonDesc = {
-                "extractors" : [
-                {
-                  "type" : "TFIDFSimilarity",
-                  "params" : {
-                    "fieldName" : "text",
-                    "similType" : "bm25",
-                    "k1"        : k1str,
-                    "b"         : bstr
+      jsonDesc = {
+                  "extractors" : [
+                  {
+                    "type" : "TFIDFSimilarity",
+                    "params" : {
+                      "fieldName" : "text",
+                      "similType" : "bm25",
+                      "k1"        : k1str,
+                      "b"         : bstr
+                    }
                   }
-                }
-                ]
-                }
-    jsonFileName = fid + '.json'
-    jsonExtrPath = os.path.join(outDir, jsonFileName)
+                  ]
+                  }
 
-    descDataJSON.append({EXPER_SUBDIR_PARAM : os.path.join(relDescDir, 'bm25tune', fid),
-                         EXTR_TYPE_PARAM : jsonExtrPath})
+      yield fid, jsonDesc
 
-    with open(jsonExtrPath, 'w') as f:
-      json.dump(jsonDesc, f)
+parser = BaseParser('BM25 tuning param generator')
+
+genDescriptors(parser.args, extrJsonGen, 'bm25tune.json', 'bm25tune')
 
 
-with open(os.path.join(outDescDir, 'bm25tune.json'), 'w') as of:
-  json.dump(descDataJSON, of, indent=2)
-      
