1fe5cfacac7367d742208a062288c02bc7323746
Boytsov <leonid.boytsov@us.bosch.com>
2020-12-09 13:48:34 -0500

    fix installation script.

--- install_packages.sh
+++ install_packages.sh
@@ -10,15 +10,14 @@ source scripts/config.sh
 USE_MATCHZOO=0
 
 plist=(\
-urllib           "" \
-lxml             "" \
-pytools          "" \
-torch            "1.4" \
-torchtext        "0.6.0" \
-numpy            "" \
-bs4              "" \
-thrift           "0.13.0" \
-spacy            "2.2.3" \
+lxml             "" 
+pytools          "" 
+torch            "1.4" 
+torchtext        "0.6.0" 
+numpy            "1.18.2" 
+bs4              "" 
+thrift           "0.13.0" 
+spacy            "2.2.3" 
 )
 
 if [ "$USE_MATCHZOO" = "1" ] ; then
@@ -35,25 +34,13 @@ echo ${plist[*]}
 if [ -d pytorch-pretrained-BERT-mod ] ; then
   rm -rf pytorch-pretrained-BERT-mod
 fi
-
-git clone https://github.com/searchivarius/pytorch-pretrained-BERT-mod
-cd pytorch-pretrained-BERT-mod
-python setup.py install
-cd ..
-
-rm -rf mgiza
-git clone https://github.com/moses-smt/mgiza.git
-cd mgiza/mgizapp
-cmake .
-make -j 4
-make install
 cd $curr_dir
 
 qty=${#plist[*]}
 for ((i=0;i<$qty;i+=2)) ; do
   pname=${plist[$i]}
   ver=${plist[$i+1]}
-  echo "$pname -> $ver"
+  echo "Installing package $pname -> $ver"
   if [ "$ver" = "" ] ; then
     pip install "$pname"
   else
@@ -64,13 +51,26 @@ done
 
 python -m spacy download en_core_web_sm
 
-cd trec_eval 
-make  
+# This should be installed after numpy or else it will try to isntall an incompatible version!
 cd $curr_dir
+git clone https://github.com/searchivarius/pytorch-pretrained-BERT-mod
+cd pytorch-pretrained-BERT-mod
+python setup.py install
 
+cd $curr_dir
+rm -rf mgiza
+git clone https://github.com/moses-smt/mgiza.git
+cd mgiza/mgizapp
+cmake .
+make -j 4
+make install
+
+cd $curr_dir/trec_eval 
+make  
+
+cd $curr_dir
 cd lemur-code-r2792-RankLib-trunk >/dev/null
 mvn clean package 
 cp target/RankLib-2.14.jar ../lib/umass/RankLib/2.14.fixed/RankLib-2.14.fixed.jar
-cd $curr_dir
 
 echo "All is installed!"
