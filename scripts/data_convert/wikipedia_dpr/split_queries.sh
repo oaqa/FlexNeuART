03633d6d5ab3902581367a300584e99d19eb863c
searchivarius <leo@boytsov.info>
2020-12-18 00:09:01 -0500

    some script simplifications.

--- scripts/data_convert/wikipedia_dpr/split_queries.sh
+++ scripts/data_convert/wikipedia_dpr/split_queries.sh
@@ -8,6 +8,8 @@ checkVarNonEmpty "QUESTION_FILE"
 checkVarNonEmpty "QREL_FILE"
 checkVarNonEmpty "DERIVED_DATA_SUBDIR"
 
+set -oe pipefail
+
 oldCollPref=$1;
 if [ "$oldCollPref" = "" ] ; then
   echo "Specify the prefix name of the current collection (1st arg)"
@@ -15,7 +17,7 @@ if [ "$oldCollPref" = "" ] ; then
 fi
 
 newCollPref=$2;
-if [ "$oldCollPref" = "" ] ; then
+if [ "$newCollPref" = "" ] ; then
   echo "Specify the prefix name of the new splitted collection (2nd arg)"
   exit 1
 fi
@@ -30,11 +32,13 @@ for colType in nq trivia squad ; do
   mkdir -p $newCollectDir
 
   cp -r ${oldCollectDir}/dev ${newCollectDir}/dev_official
-  cp -r ${oldCollectDir}/pass ${newCollectDir}/pass
+  #cp -r ${oldCollectDir}/pass ${newCollectDir}/pass
 
   python -u scripts/data_convert/wikipedia_dpr/split_queries.py \
                 --src_dir "${oldCollectDir}/train" \
                 --dst_dir "$newCollectDir" \
-                --partitions_sizes="-1,5000,5000" \
-                --partitions_names "bitext,train_fusion,dev"
+                --partitions_sizes="-1,5000" \
+                --partitions_names "bitext,dev"
+#                --partitions_sizes="-1,5000,5000" \
+#                --partitions_names "bitext,train_fusion,dev"
 done
