9313b9f0336f4f347ad2ebdcac0ba33c2b878933
searchivarius <leo@boytsov.info>
2021-02-02 01:01:24 -0500

    simplifying the caching candidate generator.

--- src/main/java/edu/cmu/lti/oaqa/flexneuart/cand_providers/TrecRunCandidateProvider.java
+++ src/main/java/edu/cmu/lti/oaqa/flexneuart/cand_providers/TrecRunCandidateProvider.java
@@ -30,67 +30,27 @@ import edu.cmu.lti.oaqa.flexneuart.utils.EvalUtils;
 
 /**
  * A candidate provider that reads from a previously generated TREC-style run.
- * The run should be placed into the same directory with a query file. Furthermore,
- * 1. the input query is supposed match exactly one of the provided queries.
- *    when there's no match, a warning is produced.
- * 2. all queries are IDs must be unique
- * 3. the query is matched to the respective run using a query ID.
+ * The results are matched using a query ID, so it must be exactly the same as in 
+ * the query that originally generated that TREC run.
  * 
  * @author Leonid Boytsov
  *
  */
 public class TrecRunCandidateProvider extends CandidateProvider {
   final static Logger logger = LoggerFactory.getLogger(TrecRunCandidateProvider.class);
-
-  public final static String QUERY_FILE_PARAM = "queryFile";
-  public final static String TREC_RUN_FILE_PARAM = "trecRunFile";
   
   /***
    * Constructor.
    * 
-   * @param trecRunDirName  the name of the directory with a question file
-   * @param addConf
+   * @param trecFileName  the name of the trec run file (can be gz or bz2 compressed)
+   * 
    * @throws Exception
    */
-  public TrecRunCandidateProvider(String trecRunDirName, CandProvAddConfig addConf) throws Exception {
-    String queryFileName = addConf.getReqParamStr(QUERY_FILE_PARAM);
-    String trecRunFileName = addConf.getReqParamStr(TREC_RUN_FILE_PARAM);
+  public TrecRunCandidateProvider(String trecFileName) throws Exception {
+
+    mTrecRuns = EvalUtils.readTrecResults(trecFileName);
     
-    String fullQueryFile = new File(trecRunDirName, queryFileName).getAbsolutePath();
-    
-    int queryQty = 0;
-    try (DataEntryReader inp = new DataEntryReader(fullQueryFile)) {
-      Map<String, String> queryFields = null;      
-      
-      while ((queryFields = inp.readNext()) != null) {
-         
-        String text = queryFields.get(Const.TEXT_FIELD_NAME);
-        String queryId = queryFields.get(Const.TAG_DOCNO);
-        
-        if (queryId == null) {
-          logger.info("Null queryID, query #: " + (queryQty+1));
-          continue;
-        }
-        
-        if (text == null) {
-          logger.info("Null text query ID: " + queryId);
-          continue;
-        }
-        
-        mQueryTextToId.put(text.trim(), queryId);
-        
-        ++queryQty;
-        if (queryQty % 1000 == 0) logger.info("Read " + queryQty + " documents from " + fullQueryFile);
-      }
-    }
-    
-    logger.info("Read " + queryQty + " documents from " + fullQueryFile);
-    
-    String fullTrecRunFile = new File(trecRunDirName, trecRunFileName).getAbsolutePath();
-    
-    mTrecRuns = EvalUtils.readTrecResults(fullTrecRunFile);
-    
-    logger.info("Read " + mQueryTextToId.size() + " queries from " + fullTrecRunFile);
+    logger.info("Read " + mQueryTextToId.size() + " queries from " + trecFileName);
   }
 
   @Override
@@ -112,23 +72,11 @@ public class TrecRunCandidateProvider extends CandidateProvider {
           String.format("Query id (%s) is undefined for query # %d",
                         ID_FIELD_NAME, queryNum));
     }        
-    
-    String text = queryData.get(QUERY_FIELD_NAME);
-    if (null == text) {
-      throw new Exception(
-          String.format("Query (%s) is undefined for query # %d",
-                        QUERY_FIELD_NAME, queryNum));
-    }
-    text = text.trim();
-    String cacheQueryID = mQueryTextToId.get(text);
-    if (cacheQueryID == null) {
-      logger.warn("Ignoring  query " + queryID + " because matching by text failed");
-      return mEmpty;
-    }
-    ArrayList<CandidateEntry> resArr = mTrecRuns.get(cacheQueryID);
+  
+    ArrayList<CandidateEntry> resArr = mTrecRuns.get(queryID);
     if (resArr == null) {
       logger.warn("Ignoring  query " + queryID + 
-                 " because trec run contains not entry for cached query ID " + cacheQueryID);
+                 " because trec run contains not entry for cached query ID " + queryID);
       return mEmpty;
     }
     
