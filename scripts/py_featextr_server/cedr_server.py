5272d11259fcb9f1f1a1ba878c56397470042b1d
searchivarius <leo@boytsov.info>
2021-02-14 18:12:46 -0500

    supporting multiple models.

--- scripts/py_featextr_server/cedr_server.py
+++ scripts/py_featextr_server/cedr_server.py
@@ -18,7 +18,7 @@ class CedrQueryHandler(BaseQueryHandler):
     # Exclusive==True means that only one getScores
     # function is executed at at time
     def __init__(self,
-                    model,
+                    modelList, modelWeightList,
                     batchSize, deviceName,
                     maxQueryLen, maxDocLen,
                     exclusive,
@@ -33,12 +33,24 @@ class CedrQueryHandler(BaseQueryHandler):
         self.deviceName = deviceName
         print('Maximum query/document len %d/%d device: %s' % (self.maxQueryLen, self.maxDocLen, self.deviceName))
 
-        self.model = model
+        self.modelList = modelList
+        for model in self.modelList:
+            model.to(self.deviceName)
+            # need to be in the eval mode
+            model.eval()
 
-        self.model.to(self.deviceName)
+        # This covers both None and empty list
+        if not modelWeightList:
+            modelWeightList = [1.0] * len(modelList)
 
-        # need to be in the eval mode
-        self.model.eval()
+        assert len(modelWeightList) == len(modelList), "The weight list should be empty or have the same # of elements as the model list!"
+        # Normalize weights
+        weightSum = sum(modelWeightList)
+        assert weightSum > 0, "The sum of the weights should be positive!"
+
+        self.modelWeightList = [float(w) / weightSum for w in modelWeightList]
+        print('Normalized model weights:', self.modelWeightList)
+
 
     def computeScoresFromParsedOverride(self, query, docs):
         queryRaw = TextEntryRaw(query.id, self.concatTextEntryWords(query))
@@ -67,24 +79,30 @@ class CedrQueryHandler(BaseQueryHandler):
             dataSet = queryData, docData
             # must disable gradient computation to greatly reduce memory requirements and speed up things
             with torch.no_grad():
-                for records in data.iter_valid_records(self.model, self.deviceName, dataSet, run,
-                                                       self.batchSize,
-                                                       self.maxQueryLen, self.maxDocLen):
-                    scores = self.model(records['query_tok'],
+                for model, weight in zip(self.modelList, self.modelWeightList):
+                    for records in data.iter_valid_records(model, self.deviceName, dataSet, run,
+                                                           self.batchSize,
+                                                           self.maxQueryLen, self.maxDocLen):
+
+                        scores = model(records['query_tok'],
                                         records['query_mask'],
                                         records['doc_tok'],
                                         records['doc_mask'])
 
-                    # tolist() works much faster compared to extracting scores
-                    # one by one using .item()
-                    scores = scores.tolist()
 
-                    for qid, did, score in zip(records['query_id'], records['doc_id'], scores):
-                        if self.debugPrint:
-                            print(score, did, docData[did])
-                        # Note that each element must be an array, b/c
-                        # we can generate more than one feature per document!
-                        sampleRet[did] = [score]
+                        # tolist() works much faster compared to extracting scores
+                        # one by one using .item()
+                        scores = (weight * scores).tolist()
+
+                        for qid, did, score in zip(records['query_id'], records['doc_id'], scores):
+                            if self.debugPrint:
+                                print(score, did, docData[did])
+                            # Note that each element must be an array, b/c
+                            # one can potentially generate more than one feature per document!
+                            if did in sampleRet:
+                                sampleRet[did][0] += score
+                            else:
+                                sampleRet[did] = [score]
 
         if self.debugPrint:
             print('All scores:', sampleRet)
@@ -92,10 +110,49 @@ class CedrQueryHandler(BaseQueryHandler):
         return sampleRet
 
 
+def add_eval_model_init_args(parser):
+
+    parser.add_argument('--model', metavar='model',
+                        help='a model to use: ' + ' '.join(list(model_init_utils.MODEL_MAP.keys())),
+                        choices=model_init_utils.MODEL_MAP.keys(), default='vanilla_bert')
+
+    parser.add_argument('--init_model_weights',
+                        metavar='model weights', help='initial model weights',
+                        type=argparse.FileType('rb'),
+                        nargs='+',
+                        default=None)
+
+    parser.add_argument('--init_model',
+                        metavar='initial model',
+                        help='initial *COMPLETE* model with heads and extra parameters',
+                        type=argparse.FileType('rb'),
+                        nargs='+',
+                        default=None)
+
+    parser.add_argument('--model_mix_weights',
+                        metavar='model mixing weights',
+                        help='weights to linearly combine model scores (same weights by default)',
+                        type=float,
+                        nargs='+',
+                        default=None)
+
+    parser.add_argument('--max_query_len', metavar='max. query length',
+                        type=int, default=data.DEFAULT_MAX_QUERY_LEN,
+                        help='max. query length')
+
+    parser.add_argument('--max_doc_len', metavar='max. document length',
+                        type=int, default=data.DEFAULT_MAX_DOC_LEN,
+                        help='max. document length')
+
+
+    parser.add_argument('--device_name', metavar='CUDA device name or cpu', default='cuda:0',
+                        help='The name of the CUDA device to use')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Serving CEDR models.')
 
-    model_init_utils.add_model_init_basic_args(parser, False)
+    add_eval_model_init_args(parser)
 
     parser.add_argument('--debug_print', action='store_true',
                         help='Provide debug output')
@@ -115,24 +172,39 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    model_list = []
+
+    assert(args.init_model is None or type(args.init_model) == list)
+    assert (args.init_model_weights is None or type(args.init_model_weights) == list)
+    assert (args.model_mix_weights is None or type(args.model_mix_weights) == list)
+
+    modelList = []
+
     if args.init_model is None:
+        # TODO this one isn't properly tested
         if args.model is not None and args.init_model_weights is not None:
-            model = model_init_utils.create_model_from_args(args)
-            print('Loading model weights from:', args.init_model_weights.name)
-            # If we load weights here, we must set strict to True:
-            # this would prevent accidental loading of partial models.
-            # Partial models are sure fine to load during training (as a reasonable
-            # initialization), but not during test time.
-            model.load_state_dict(torch.load(args.init_model_weights.name, map_location='cpu'),
-                                  strict=True)
+            for fname in args.init_model_weights.name:
+                model = model_init_utils.create_model_from_args(args)
+                print('Loading model weights from:', fname.name)
+                # If we load weights here, we must set strict to True:
+                # this would prevent accidental loading of partial models.
+                # Partial models are sure fine to load during training (as a reasonable
+                # initialization), but not during test time.
+                model.load_state_dict(torch.load(fname.name, map_location='cpu'),
+                                      strict=True)
+                modelList.append(model)
         else:
             print('Specify the model file: --init_model or model type and model weights')
             sys.exit(1)
     else:
-        model = torch.load(args.init_model, map_location='cpu')
+        for fname in args.init_model:
+            print('Loading model from:', fname.name)
+            model = torch.load(fname.name, map_location='cpu')
+            modelList.append(model)
 
     multiThreaded = False  # if we set to True, we can often run out of CUDA memory.
-    startQueryServer(args.host, args.port, multiThreaded, CedrQueryHandler(model=model,
+    startQueryServer(args.host, args.port, multiThreaded, CedrQueryHandler(modelList=modelList,
+                                                                           modelWeightList=args.model_mix_weights,
                                                                            batchSize=args.batch_size,
                                                                            debugPrint=args.debug_print,
                                                                            deviceName=args.device_name,
