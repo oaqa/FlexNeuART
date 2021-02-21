e129e807d8f653e51f57e41b84f12fbc461b1825
searchivarius <leo@boytsov.info>
2021-02-20 19:01:48 -0500

    ...

--- scripts/export_train/gen_pseudo_labels.py
+++ scripts/export_train/gen_pseudo_labels.py
@@ -9,13 +9,13 @@ sys.path.append('.')
 
 from scripts.common_eval import readRunDict, writeQrels, QrelEntry, getSorteScoresFromScoreDict
 
-parser = argparse.ArgumentParser('Exporting a neural Model1 model to a GIZA format (to run on CPU)')
+parser = argparse.ArgumentParser('Generate pseudo-QRELs from a run')
 
 parser.add_argument('--input_run', metavar='input run file',
                     required=True, type=str, help='input run file')
 parser.add_argument('--out_qrels', metavar='output QREL file',
                     required=True, type=str, help='output QREL file')
-parser.add_argument('--top_k', metavar='top k',
+parser.add_argument('--top_k', metavar='top k', default=1,
                     required=True, type=int, help='top k entries to use as psedo relevant labels')
 parser.add_argument('--grade', metavar='grade',
                     default=1, type=int, help='a grade for the relevance item')
