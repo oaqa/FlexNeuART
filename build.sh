3a6efc954f2c90f9435b99b601d8d4b32507ff55
searchivarius <leo@boytsov.info>
2021-05-01 12:44:50 -0400

    Adding maven -U option that forces some package checks and downloads that won't happen otherwise.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ log_file="$curr_dir/main.build.log"
 echo "========================"
 echo " BUILDING main codebase "
 echo " log: $log_file"
-mvn clean package appassembler:assemble &> $log_file || { echo "Build failed!" ; exit 1 ; }
+mvn -U clean package appassembler:assemble &> $log_file || { echo "Build failed!" ; exit 1 ; }
 
 echo "======================="
 echo "BUILD IS COMPLETE!"
