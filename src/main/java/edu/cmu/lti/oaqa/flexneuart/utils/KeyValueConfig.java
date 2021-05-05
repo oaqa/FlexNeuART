a1d5b9c82c362a59a6ac5b269016646cad9eb8e1
searchivarius <leo@boytsov.info>
2021-05-05 18:20:33 -0400

    -cryptic parsing messages in Java configs.

--- src/main/java/edu/cmu/lti/oaqa/flexneuart/utils/KeyValueConfig.java
+++ src/main/java/edu/cmu/lti/oaqa/flexneuart/utils/KeyValueConfig.java
@@ -25,27 +25,43 @@ public abstract class KeyValueConfig {
     String val = params.get(name);
     if (val == null)
       throw new Exception(String.format("Mandatory parameter %s is undefined for " + getName(), name));
-    return Float.parseFloat(val);
+    try {
+      return Float.parseFloat(val);
+    } catch (NumberFormatException e) {
+      throw new Exception("Parameter '" + name + "' is not a float!");
+    }
   } 
   
   public int getReqParamInt(String name) throws Exception {
     String val = params.get(name);
     if (val == null)
       throw new Exception(String.format("Mandatory parameter %s is undefined for " + getName(), name));
-    return Integer.parseInt(val);
+    try {
+      return Integer.parseInt(val);
+    } catch (NumberFormatException e) {
+      throw new Exception("Parameter '" + name + "' is not an int!");
+    }
   }
   
   public boolean getReqParamBool(String name) throws Exception {
     String val = params.get(name);
     if (val == null)
       throw new Exception(String.format("Mandatory parameter %s is undefined for " + getName(), name));
-    return Boolean.parseBoolean(val);
+    try {
+      return Boolean.parseBoolean(val);
+    } catch (NumberFormatException e) {
+      throw new Exception("Parameter '" + name + "' is not a boolean (true/false)!");
+    }
   } 
   
   public boolean getParamBool(String name) throws Exception {
     String val = params.get(name);
     if (val == null) return false;
-    return Boolean.parseBoolean(val);
+    try {
+      return Boolean.parseBoolean(val);
+    } catch (NumberFormatException e) {
+      throw new Exception("Parameter '" + name + "' is not a boolean (true/false)!");
+    }
   }
   
   public String getParam(String name, String defaultValue) {
@@ -53,19 +69,31 @@ public abstract class KeyValueConfig {
     return val != null ? val : defaultValue;
   }
   
-  public float getParam(String name, float defaultValue) {
+  public float getParam(String name, float defaultValue) throws Exception {
     String val = params.get(name);
-    return val != null ? Float.parseFloat(val) : defaultValue;
+    try {
+      return val != null ? Float.parseFloat(val) : defaultValue;
+    } catch (NumberFormatException e) {
+      throw new Exception("Parameter '" + name + "' is not a float!");
+    }
   } 
   
-  public int getParam(String name, int defaultValue) {
+  public int getParam(String name, int defaultValue) throws Exception {
     String val = params.get(name);
-    return val != null ? Integer.parseInt(val) : defaultValue;
+    try {
+      return val != null ? Integer.parseInt(val) : defaultValue;
+    } catch (NumberFormatException e) {
+      throw new Exception("Parameter '" + name + "' is not an int!");
+    }
   }
   
-  public boolean getParam(String name, boolean defaultValue) {
+  public boolean getParam(String name, boolean defaultValue) throws Exception {
     String val = params.get(name);
-    return val != null ? Boolean.parseBoolean(val) : defaultValue;
+    try {
+      return val != null ? Boolean.parseBoolean(val) : defaultValue;
+    } catch (NumberFormatException e) {
+      throw new Exception("Parameter '" + name + "' is not a boolean (true/false)!");
+    }
   }
 
 }
