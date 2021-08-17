0f48d0022c4efd8d343b8d5edf6540f4f9f09073
searchivarius <leo@boytsov.info>
2021-08-18 02:39:14 +0300

    adding TCT-COLBERT passage encoders

---
+++ scripts/data_convert/tct_colbert_pass_v1/embed.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+#
+#  Copyright 2014+ Carnegie Mellon University
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+# A script to convert MS MARCO v1 passages or queries to dense vectors using PySerini's TCT-COLBERT models
+# These vectors are stored in "BSONL" format, which can be used to create a forward index.
+#
+
+import argparse
+import os
+import sys
+import torch
+import numpy as np
+from tqdm import tqmd
+
+DEVICE_NAME='cuda'
+
+sys.path.append('.')
+
+from scripts.config import TEXT_RAW_FIELD_NAME
+from scripts.data_convert.convert_common import FileWrapper, DOCID_FIELD, pack_dense_batch, \
+                                                write_json_to_bin, is_json_query_file, jsonl_gen
+from scripts.data_convert.tct_colbert_pass_v1.tct_colbert_models import TctColBertDocumentEncoder, TctColBertQueryEncoder
+
+
+parser = argparse.ArgumentParser(description='Convert passages and/or documents to dense vectors and store them in "BSONL" format.')
+
+parser.add_argument('--input', metavar='input file',
+                    help='input file (query JSONL or raw passage/document input file)',
+                    type=str, required=True)
+parser.add_argument('--batch_size', metavar='batch size', help='batch size',
+                    type=int, default=16)
+parser.add_argument('--output', metavar='output file', help='output file',
+                    type=str, required=True)
+parser.add_argument('--field_name', metavar='field name', help='the name of the BSONL field',
+                    type=str, required=True)
+parser.add_argument('--model', metavar='model name or path',
+                    help='a directory with downloaded and unpacked models',
+                    type=str, default="castorini/tct_colbert-msmarco")
+parser.add_argument('--fp16', action='store_true', help="Use half-precision")
+
+args = parser.parse_args()
+
+print('Model', args.model)
+
+is_query = is_json_query_file(args.input)
+
+print(f'Query?: {is_query}')
+
+if is_query:
+    model = TctColBertQueryEnecoder(args.model, fp16=args.fp16, device=DEVICE_NAME)
+else:
+    model = TctColBertDocumentEncoder(args.model, device=DEVICE_NAME)
+
+
+batch_input = []
+
+def proc_batch(batch_input, model, out_file, field_name):
+    if batch_input:
+        doc_ids, texts = zip(*batch_input)
+
+        bqty = len(batch_input)
+
+        batch_data = model.encode(texts)
+        print(batch_data.shape)
+
+        batch_data_packed = pack_dense_batch(batch_data)
+
+        assert len(batch_data_packed) == bqty
+
+        for i in range(bqty):
+            data_elem = {DOCID_FIELD : doc_ids[i], field_name : batch_data_packed[i]}
+            write_json_to_bin(data_elem, out_file)
+
+        batch_input.clear()
+
+
+with FileWrapper(args.output, 'wb') as out_file:
+    for e in tqdm(jsonl_gen(args.input)):
+        doc_id = e[DOCID_FIELD]
+        text = e[TEXT_RAW_FIELD_NAME]
+
+        batch_input.append((doc_id, text))
+
+        if len(batch_input) >= args.batch_size:
+            proc_batch(batch_input, model, out_file, args.field_name)
+
+    proc_batch(batch_input, model, out_file, args.field_name)
---
+++ scripts/data_convert/tct_colbert_pass_v1/tct_colbert_models.py
@@ -0,0 +1,98 @@
+#
+# Slightly modified code from  pyserini/encode/_tct_colbert.py, pyserini/encode/_base.py
+# ONNX support removed
+#
+# Pyserini: Reproducible IR research with sparse and dense representations
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import numpy as np
+import torch
+from torch.cuda.amp import autocast
+from transformers import BertModel, BertTokenizer, BertTokenizerFast
+
+class DocumentEncoder:
+    def encode(self, texts, **kwargs):
+        pass
+
+    @staticmethod
+    def _mean_pooling(last_hidden_state, attention_mask):
+        token_embeddings = last_hidden_state
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+        sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        return sum_embeddings / sum_mask
+
+class QueryEncoder:
+    def encode(self, text, **kwargs):
+        pass
+
+
+class TctColBertDocumentEncoder(DocumentEncoder):
+    def __init__(self, model_name: str, device, fp16=False, tokenizer_name=None):
+        self.device = device
+
+        self.model = BertModel.from_pretrained(model_name)
+        self.model.to(self.device)
+        self.tokenizer = BertTokenizerFast.from_pretrained(tokenizer_name or model_name)
+        self.fp16 = fp16
+
+    def encode(self, texts, titles=None, **kwargs):
+        if titles is not None:
+            texts = [f'[CLS] [D] {title} {text}' for title, text in zip(titles, texts)]
+        else:
+            texts = ['[CLS] [D] ' + text for text in texts]
+        max_length = 512  # hardcode for now
+        inputs = self.tokenizer(
+            texts,
+            max_length=max_length,
+            padding="longest",
+            truncation=True,
+            add_special_tokens=False,
+            return_tensors='pt'
+        )
+        
+        inputs.to(self.device)
+        if self.fp16:
+            with autocast():
+                with torch.no_grad():
+                    outputs = self.model(**inputs)
+        else:
+            outputs = self.model(**inputs)
+
+        embeddings = self._mean_pooling(outputs["last_hidden_state"][:, 4:, :], inputs['attention_mask'][:, 4:])
+
+        return embeddings.detach().cpu().numpy()
+
+
+class TctColBertQueryEncoder(QueryEncoder):
+    def __init__(self, model_name: str, device, tokenizer_name: str = None):
+        self.device = device
+        self.model = BertModel.from_pretrained(model_name)
+        self.model.to(self.device)
+        self.tokenizer = BertTokenizer.from_pretrained(tokenizer_name or model_name)
+
+    def encode(self, query: str, **kwargs):
+        max_length = 36  # hardcode for now
+        inputs = self.tokenizer(
+            '[CLS] [Q] ' + query + '[MASK]' * max_length,
+            max_length=max_length,
+            truncation=True,
+            add_special_tokens=False,
+            return_tensors='pt'
+        )
+        inputs.to(self.device)
+        outputs = self.model(**inputs)
+        embeddings = outputs.last_hidden_state.detach().cpu().numpy()
+        return np.average(embeddings[:, 4:, :], axis=-2).flatten()
