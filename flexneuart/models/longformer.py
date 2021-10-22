a6cf543512403aa07167c14802f16a1216f5c9c3
searchivarius <leo@boytsov.info>
2021-10-21 23:29:58 -0400

    simplifications

--- flexneuart/models/longformer.py
+++ flexneuart/models/longformer.py
@@ -1,10 +1,6 @@
 import torch
-import torch.nn.functional as F
 
-from flexneuart.config import BERT_BASE_MODEL
 from flexneuart.models import register
-from flexneuart.models.base_bert_split_slide_window import \
-        BertSplitSlideWindowRanker, DEFAULT_STRIDE, DEFAULT_WINDOW_SIZE
 from flexneuart.models.base_bert import DEFAULT_BERT_DROPOUT
 
 from transformers.modeling_outputs import BaseModelOutputWithPoolingAndCrossAttentions
@@ -18,7 +14,7 @@ class LongformerRanker(BertBaseRanker):
         This code I am going to try to base it off of the BERT one, but I am not sure if I can.
     """
 
-    def __init__(self, bert_flavor='allenai/longformer-base-4096', dropout=DEFAULT_BERT_DROPOUT, att_window_size=512):
+    def __init__(self, bert_flavor='allenai/longformer-base-4096', dropout=DEFAULT_BERT_DROPOUT):
         """Constructor.
 
             :param bert_flavor:   the name of the underlying Transformer/BERT. Various
@@ -27,7 +23,6 @@ class LongformerRanker(BertBaseRanker):
 
         """
         super().__init__(bert_flavor)
-        self.att_window_size = att_window_size # This window size should be even.
         self.dropout = torch.nn.Dropout(dropout)
         self.cls = torch.nn.Linear(self.BERT_SIZE, 1)
         torch.nn.init.xavier_uniform_(self.cls.weight)
@@ -53,25 +48,24 @@ class LongformerRanker(BertBaseRanker):
 
         """
         batch_qty, max_qlen = query_tok.shape
+        _, max_dlen = doc_tok.shape
 
-        sb_count = 1
-        sbcount = sb_count
+        max_dlen = min(max_dlen, self.MAXLEN - 3 - max_qlen)
+        assert max_dlen > 0, "Queries are too long!"
 
-        doc_toks = doc_tok
+        # If documents are too long, they need to be shortened
+        doc_toks = doc_tok[:max_dlen]
+        doc_masks = doc_mask[:max_dlen]
 
-        doc_masks = doc_mask
-
-        query_toks = torch.cat([query_tok] * sbcount, dim=0)
-        query_mask = torch.cat([query_mask] * sbcount, dim=0)
-
-        CLSS = torch.full_like(query_toks[:, :1], self.CLS_TOK_ID)
-        SEPS = torch.full_like(query_toks[:, :1], self.SEP_TOK_ID)
+        CLSS = torch.full_like(query_tok[:, :1], self.CLS_TOK_ID)
+        SEPS = torch.full_like(query_tok[:, :1], self.SEP_TOK_ID)
         ONES = torch.ones_like(query_mask[:, :1])
         NILS = torch.zeros_like(query_mask[:, :1])
 
         # build input sequences
-        toks = torch.cat([CLSS, query_toks, SEPS, doc_toks, SEPS], dim=1)
+        toks = torch.cat([CLSS, query_tok, SEPS, doc_toks, SEPS], dim=1)
         mask = torch.cat([ONES, query_mask, ONES, doc_masks, ONES], dim=1)
+
         segment_ids = torch.cat([NILS] * (2 + max_qlen) + [ONES] * (1 + doc_toks.shape[1]), dim=1)
         toks[toks == -1] = 0 # remove padding (will be masked anyway)
 
@@ -81,21 +75,12 @@ class LongformerRanker(BertBaseRanker):
                       token_type_ids=None,
                       attention_mask=mask.long(),
                       global_attention_mask = ((1-segment_ids)*mask).long(),
-                      output_hidden_states=True)
-        result = outputs.hidden_states
+                      output_hidden_states=False)
 
-        # build CLS representation
-        cls_results = []
-        last_layer = (result[-1])
-        for layer in result:
-            # Only the last layer is being processed, and we only need the CLS part of it from the 768 elements.
-            cls_output = layer[:, 0]
-            cls_results.append(cls_output)
-        return cls_results
+        return outputs.last_hidden_state
 
     def forward(self, query_tok, query_mask, doc_tok, doc_mask):
         cls_reps = self.encode_lf(query_tok, query_mask, doc_tok, doc_mask)
-        out = self.cls(self.dropout(cls_reps[-1]))
+        out = self.cls(self.dropout(cls_reps))
         # the last dimension is singleton and needs to be removed
-        print()
         return out.squeeze(dim=-1)
