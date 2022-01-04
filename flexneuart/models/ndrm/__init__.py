c134387fcc24eea5e9edc5f07adc37534a467fcb
BOL1PI <BOL1PI@bright82.cm.cluster>
2022-01-04 00:11:21 -0800

    graceful failure for NDRM models (which require extra components)

--- flexneuart/models/ndrm/__init__.py
+++ flexneuart/models/ndrm/__init__.py
@@ -1,1 +1,4 @@
-from flexneuart.models.ndrm import models_wrapper
+try:
+    from flexneuart.models.ndrm import models_wrapper
+except Exception as e:
+    print('Failed to load the NDRM models (which require additional libraries): ' + str(e))
