c647273fb488d28c31e2e705b253005f645939f6
Ramya Ramanathan <ramyaramanathan2811@gmail.com>
2022-04-29 15:11:45 -0400

    Adding changes

--- flexneuart/data_augmentation/base_class.py
+++ flexneuart/data_augmentation/base_class.py
@@ -1,5 +1,5 @@
 import random
-
+from abc import abstractmethod
 class DataAugment:
     def __init__(self, random_seed=42):
         self.random_seed = random.Random(random_seed)
--- flexneuart/data_augmentation/random_word_transformations.py
+++ flexneuart/data_augmentation/random_word_transformations.py
@@ -6,14 +6,39 @@ class RandomInsertion(DataAugment):
         words = re.split('\s+', text)
         num_words = len(words)
         number_ri = max(1, int(alpha_ri*num_words))
-		a_words = self.random_insertion(words, n_ri)
-			augmented_sentences.append(' '.join(a_words))
+        a_words = random_insertion(words, n_ri)
+        augmented_sentences.append(' '.join(a_words))
 
 
 class RandomDeletion(DataAugment):
-    def augment(text, **kwargs):
-        pass
+    def __init__(p = 0.2):
+        self.p = p
+        super().__init__()
+
+    def augment(self, text):
+        # Code referenced from: https://github.com/jasonwei20/eda_nlp/blob/master/code/eda.py
+
+        words = text.split()
+
+        # No deletion if only one word is present
+        if len(words) == 1:
+            return words
+
+        # Delete words randomly with probability p
+        new_words = []
+        for word in words:
+            r = random.uniform(0, 1)
+            if r > self.p:
+                new_words.append(word)
+
+        # Return a random word if all words deleted
+        if len(new_words) == 0:
+            rand_int = random.randint(0, len(words)-1)
+            return [words[rand_int]]
+
+        return " ".join(new_words)
+
 
 class RandomSwap(DataAugment):
-    def augment(text, **kwargs):
+    def augment(self, text, **kwargs):
         pass
