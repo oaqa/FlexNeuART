8af88e6d1cc8c269cedfde0a63d1b64ebd066831
Vivek Sourabh <viveksourabh2808@gmail.com>
2022-04-29 14:38:02 -0400

    add spacy and random object to base init

--- flexneuart/data_augmentation/base_class.py
+++ flexneuart/data_augmentation/base_class.py
@@ -1,6 +1,12 @@
+import spacy
+import random
+
 
 class DataAugment:
-    def __init__(self):
+    def __init__(self, spacy_model='en_core_web_sm', random_seed=42):
+        self.nlp = spacy.load(self.spacy_model)
+        self.random = random.Random(self.random_seed)
+
         return
 
     @abstractmethod
