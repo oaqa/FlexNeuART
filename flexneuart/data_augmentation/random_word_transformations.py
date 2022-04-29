10d954170ca4bdda18180f5ca98f0503658504e2
Ramya Ramanathan <ramyaramanathan2811@gmail.com>
2022-04-29 14:45:55 -0400

    Adding kwargs

--- flexneuart/data_augmentation/random_word_transformations.py
+++ flexneuart/data_augmentation/random_word_transformations.py
@@ -1,12 +1,12 @@
 from base_class import DataAugment
 class RandomInsertion(DataAugment):
-    def augment(text, p):
+    def augment(text, **kwargs):
         pass
 
 class RandomDeletion(DataAugment):
-    def augment(text, p):
+    def augment(text, **kwargs):
         pass
 
 class RandomSwap(DataAugment):
-    def augment(text, p):
+    def augment(text, **kwargs):
         pass
--- flexneuart/data_augmentation/synonym_hypernym_transformations.py
+++ flexneuart/data_augmentation/synonym_hypernym_transformations.py
@@ -1,8 +1,8 @@
 from base_class import DataAugment
 class SynonymReplacement(DataAugment):
-    def augment(text, p):
+    def augment(text, **kwargs):
         pass
 
 class HypernymReplacement(DataAugment):
-    def augment(text, p):
+    def augment(text, **kwargs):
         pass
