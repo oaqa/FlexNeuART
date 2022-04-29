0e1d980c815105199f9b909df7e7a43ba43bffdf
riddhinn99 <riddhinisar11@gmail.com>
2022-04-29 18:41:51 -0400

    added RandomSwap functionality

--- flexneuart/data_augmentation/utils/random_word_transformations.py
+++ flexneuart/data_augmentation/utils/random_word_transformations.py
@@ -76,5 +76,31 @@ class RandomDeletion(DataAugment):
 
 
 class RandomSwap(DataAugment):
-    def augment(self, text, **kwargs):
-        pass
+    def __init__(self, alpha_rs=0.1, random_seed=42):
+        super().__init__(random_seed)
+        self.alpha_rs = alpha_rs # percentage of random swap
+
+    def augment(self, text):
+        words = re.split('\s+', text)
+        num_words = len(words)
+        number_rs = max(1, int(self.alpha_rs*num_words))
+        augmented_words = self.random_swap(words, number_rs)
+        return ' '.join(augmented_words)
+
+    def random_swap(self, words, n):
+        new_words = words.copy()
+        for _ in range(n):
+            new_words = self.swap_word(new_words)
+        return new_words
+
+    def swap_word(self, new_words):
+        random_idx_1 = random.randint(0, len(new_words)-1)
+        random_idx_2 = random_idx_1
+        counter = 0
+        while random_idx_2 == random_idx_1:
+            random_idx_2 = random.randint(0, len(new_words)-1)
+            counter += 1
+            if counter > 3:
+                return new_words
+        new_words[random_idx_1], new_words[random_idx_2] = new_words[random_idx_2], new_words[random_idx_1] 
+        return new_words
--- flexneuart/data_augmentation/utils/random_word_transformations_test.py
+++ flexneuart/data_augmentation/utils/random_word_transformations_test.py
@@ -1,9 +1,15 @@
 # from utils import *
 import unittest
 # from transforms import *
-from random_word_transformations import RandomDeletion, RandomInsertion
+from random_word_transformations import RandomDeletion, RandomInsertion, RandomSwap
 
 class TestRandomWordTransformations(unittest.TestCase):
+    def test_random_swap(self):
+        # answer = True
+        # self.assertEqual(answer, True)
+        sentence = "This is a test sentence"
+        random_swap_sentence = RandomSwap(alpha_rs = 0.2)
+        print(random_swap_sentence.augment(sentence))
     def test_random_insertions(self):
         # answer = True
         # self.assertEqual(answer, True)
@@ -14,7 +20,6 @@ class TestRandomWordTransformations(unittest.TestCase):
         sentence = "This is a test sentence"
         random_deletion = RandomDeletion(p = 0.1)
         print(random_deletion.augment(sentence))
-        pass
 
     def test_random_deletions(self):
         text = "Testing random deletions in this sentence. Do you think words are correctly deleted"
@@ -25,3 +30,4 @@ class TestRandomWordTransformations(unittest.TestCase):
 if __name__ == "__main__":
     tester = TestRandomWordTransformations()
     tester.test_random_deletions()
+    tester.test_random_swap()
