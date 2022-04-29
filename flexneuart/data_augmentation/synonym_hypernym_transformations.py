6c5d6a3b69beba8903dd4536f15d1df7c4823048
Ramya Ramanathan <ramyaramanathan2811@gmail.com>
2022-04-29 14:43:31 -0400

    Adding test files

---
+++ flexneuart/data_augmentation/synonym_hypernym_transformations.py
@@ -0,0 +1,8 @@
+from base_class import DataAugment
+class SynonymReplacement(DataAugment):
+    def augment(text, p):
+        pass
+
+class HypernymReplacement(DataAugment):
+    def augment(text, p):
+        pass
