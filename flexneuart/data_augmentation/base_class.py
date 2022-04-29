dcf3b3ec6c9c1d001ea1b8a4c5fcfa0e58d80c5d
Vivek Sourabh <viveksourabh2808@gmail.com>
2022-04-29 14:38:50 -0400

    remove self. prefix

--- flexneuart/data_augmentation/base_class.py
+++ flexneuart/data_augmentation/base_class.py
@@ -4,8 +4,8 @@ import random
 
 class DataAugment:
     def __init__(self, spacy_model='en_core_web_sm', random_seed=42):
-        self.nlp = spacy.load(self.spacy_model)
-        self.random = random.Random(self.random_seed)
+        self.nlp = spacy.load(spacy_model)
+        self.random = random.Random(random_seed)
 
         return
 
