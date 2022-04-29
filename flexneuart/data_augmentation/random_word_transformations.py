d638f9922fc531cd527b9627c3679a668998e1a6
Ramya Ramanathan <ramyaramanathan2811@gmail.com>
2022-04-29 14:37:46 -0400

    Adding folder structure for random word techniques

---
+++ flexneuart/data_augmentation/random_word_transformations.py
@@ -0,0 +1,12 @@
+from base_class import DataAugment
+class RandomInsertion(DataAugment):
+    def augment(text, p):
+        pass
+
+class RandomDeletion(DataAugment):
+    def augment(text, p):
+        pass
+
+class RandomSwap(DataAugment):
+    def augment(text, p):
+        pass
