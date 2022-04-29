d10f2521b85c2c84a57eb2d3356f15305aaafbe3
Preksha Patel <prekshaupatel@gmail.com>
2022-04-29 14:42:50 -0400

    cleaned base_class.py

--- flexneuart/data_augmentation/base_class.py
+++ flexneuart/data_augmentation/base_class.py
@@ -1,11 +1,8 @@
-import spacy
 import random
-
 
 class DataAugment:
     def __init__(self, random_seed=42):
         self.random_seed = random.Random(random_seed)
-
         return
 
     @abstractmethod
