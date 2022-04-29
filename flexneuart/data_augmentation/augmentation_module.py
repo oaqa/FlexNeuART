0fee4ac149bc1ac2cc3a0f7103087e4205fbadd3
Preksha Patel <prekshaupatel@gmail.com>
2022-04-29 18:28:49 -0400

    experimental setup

--- flexneuart/data_augmentation/augmentation_module.py
+++ flexneuart/data_augmentation/augmentation_module.py
@@ -1,11 +1,11 @@
 import random
 from abc import abstractmethod
-
+from flexneuart.data_augmentation.utils.abnirml_transformation import *
 
 class DataAugmentModule:
     def __init__(self, random_seed=42):
         self.random_seed = random.Random(random_seed)
-
+        
     @abstractmethod
     def augment(self, query_text, doc_text):
         pass
@@ -14,8 +14,7 @@ class DataAugmentModule:
 class RandomDataAugmentModule(DataAugmentionModule):
     def __init__(self, random_seed=42):
         super().__init__(random_seed)
-        self.query_augment = None
-        self.doc_augment = None
+        self.doc_augment = ShufSents()
 
     def augment(self, query_text, doc_text):
-        return self.query_augment(query_text), self.doc_augment(doc_text)
+        return query_text, self.doc_augment(doc_text)
