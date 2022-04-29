ee56631b2dbf3037f16f8ddff3e759051819e46b
Vivek Sourabh <viveksourabh2808@gmail.com>
2022-04-29 15:07:17 -0400

    change random seed definition

--- flexneuart/data_augmentation/base_class.py
+++ flexneuart/data_augmentation/base_class.py
@@ -4,7 +4,7 @@ import random
 
 class DataAugment:
     def __init__(self, random_seed=42):
-        self.random_seed = random.Random(random_seed)
+        self.random_seed = random_seed
 
         return
 
