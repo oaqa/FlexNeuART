c0e752f6e1b1da1e65f6bab8c1ef95510eba9642
Sayani <sayanik@andrew.cmu.edu>
2022-04-29 20:47:05 -0400

    Adding in add content structure

--- flexneuart/data_augmentation/augmentation_module.py
+++ flexneuart/data_augmentation/augmentation_module.py
@@ -3,6 +3,7 @@ from abc import abstractmethod
 from flexneuart.data_augmentation.utils.abnirml_transformation import *
 from flexneuart.data_augmentation.utils.random_word_transformations import *
 from flexneuart.data_augmentation.utils.synonym_hypernym_transformations import *
+from flexneuart.data_augmentation.utils.document_level_transformation import *
 
 class DataAugmentModule:
     def __init__(self, augment_type):
@@ -11,6 +12,8 @@ class DataAugmentModule:
             self.doc_augment = RandomWordDeletion(p = 0.05)
         elif augment_type == 'random_word_insertion':
             self.doc_augment = RandomWordInsertion(alpha_ri = 0.05)
+        # elif augment_type == 'constant_document_length':
+        #     self.doc_augment = ConstantDocLength(doc_length=500) # doc length needs to be decided
         
     def augment(self, query_text, doc_text):
         if self.doc_augment is not None:
