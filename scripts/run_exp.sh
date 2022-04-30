9fb229600e015107b8f1b41b82c909d120afbff9
Ubuntu <ubuntu@ip-172-31-83-140.ec2.internal>
2022-04-30 02:21:40 +0000

    update run exp

--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a arr=("random_word_deletion" "random_word_insert" "random_word_swap" "random_character_replace" "keyboard_character_replace")
+declare -a arr=("random_word_deletion" "random_word_insert" "random_word_swap" "random_character_replace" "keyboard_character_replace" "synonym_word_replacement" "random_character_insertion" "random_character_deletion")
 for aug_method in "${arr[@]}"
 do 
     bash train_nn/train_model.sh msmarco_pass cedr_train_pass_50K_200_0_5_0_s1_bitext/text_raw vanilla_bert \
