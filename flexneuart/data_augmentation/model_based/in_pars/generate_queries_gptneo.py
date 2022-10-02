371544423d2286e9ddc5054af92fa8027b41e98e
sayani-kundu <90480493+sayani-kundu@users.noreply.github.com>
2022-10-02 01:09:30 -0400

    Add files via upload

---
+++ flexneuart/data_augmentation/model_based/in_pars/generate_queries_gptneo.py
@@ -0,0 +1,221 @@
+import argparse
+import json
+import os
+import re
+# import openai
+import time
+# from tqdm import tqdm
+from transformers import pipeline
+import torch
+from transformers import GPT2Tokenizer, GPTNeoForCausalLM
+from torch.utils.data import Dataset, DataLoader
+import csv
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+class InParsDataset(Dataset):
+    def __init__(self, doc_file, prompt_path, max_examples):
+        self.doc_file = doc_file
+        self.max_examples = max_examples
+        self.get_docs()
+        prompt_file = open(prompt_path)
+        self.prompt = prompt_file.read()
+        prompt_file.close()
+
+
+    # Getting max examples
+    def get_docs(self):
+        
+        all_docs_input = list()
+        with open(self.doc_file,"r") as file:
+            tsv_file = csv.reader(file, delimiter="\t")
+            counter = 0
+            for i,line in enumerate(tsv_file):
+                if self.max_examples and counter==self.max_examples:
+                    break
+                if line[0]=="query":
+                    continue
+                counter+=1
+                all_docs_input.append((line[1], line[2].strip()))                
+        
+        self.sents = all_docs_input
+    
+    def __len__(self):
+        return len(self.sents)
+    
+    def __getitem__(self, index):
+        return self.sents[index][0], self.prompt.format(document_text=self.sents[index][1])
+
+class InParsCollater(object):
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+    def __call__(self, batch):
+        
+        batch_texts = [i[1] for i in batch]
+        
+        batch_ids = [i[0] for i in batch]
+        batch_lengths = [len(i) for i in batch_texts]
+        return batch_ids, tokenizer.batch_encode_plus(batch_texts, return_tensors='pt',padding=True), batch_lengths
+
+
+class GPTNeoInitialization:
+    def __init__(self):
+        pass
+    def tokenizer(self):
+        return GPT2Tokenizer
+    def base_model(self):
+        return GPTNeoForCausalLM
+
+class BloomInitialization:
+    def __init__(self):
+           pass
+    def tokenizer(self):
+        return BloomTokenizerFast
+    def base_model(self):
+        return BloomForCausalLM
+
+
+class ModelInitialization:
+
+    def __init__(self, model_name:str):
+        self.model_name = model_name
+        self.model_map = { "EleutherAI/gpt-neo-125M": GPTNeoInitialization,
+                           "EleutherAI/gpt-neo-1.3B": GPTNeoInitialization,
+                           "EleutherAI/gpt-neo-2.7B": GPTNeoInitialization,
+                           "bloom" : BloomInitialization }
+
+    def initialize_model(self):
+        initilizer_class = self.model_map[self.model_name]()
+        return initilizer_class.tokenizer(), initilizer_class.base_model()
+
+
+def write_to_output(syn_query_list,syn_query_probs, did_list , aug_query_tsv_op, aug_query_qrels_op, timestamp, counter):
+    try:
+        with open(aug_query_tsv_op, 'a') as aug_query_tsv_op, open(aug_query_qrels_op, 'a') as aug_query_qrels_op:
+            tsv_writer = csv.writer(aug_query_tsv_op, delimiter='\t')
+            for query_text, query_prob, doc_id in zip(syn_query_list, syn_query_probs, did_list):
+                new_qid = "QP" + str(timestamp) + '_' + str(counter)
+
+                tsv_writer.writerow(["query", new_qid, query_text, query_prob])
+
+                line = new_qid + " 0 " + str(doc_id) + " 1\n"
+                aug_query_qrels_op.write(line)
+
+                counter += 1
+        return counter
+    except IOError as e:
+        print(e)
+
+def gather_2d_on_last_dim(
+        tensor: FloatTensor,
+        index: LongTensor,
+        shape: torch.Size
+):
+    
+    flattened_tensor = tensor.view(-1, tensor.shape[-1])
+    flattened_index = index.view(-1)
+    flattened_gathered_tensor = flattened_tensor[
+        torch.arange(flattened_index.shape[0]),
+        flattened_index]
+    return flattened_gathered_tensor.view(shape)
+
+def postprocess_queries(generated_texts, prompt_lengths, model_outputs, question_mark_ind):
+
+    #suppose we have the model_output
+
+    
+    questions = [text[ text.find('Example 1:') + prompt_length:] for text, prompt_length in zip(generated_texts,prompt_lengths)]
+    q_inds = [q.find("?") for q in questions]
+    final_queries = [q[:q_ind+1] for q,q_ind in zip(questions,q_inds)]
+
+    valid_query_indices = [i if q!="" for i,q in enumerate(final_queries)]
+
+    #probs
+
+    probs = torch.stack(outputs.scores, dim=1).log_softmax(-1) # batchsize*tokensize*vocabsize
+
+    
+
+    length_input = input_ids.shape[1]
+    output_ids = model_output["sequences"][valid_query_indices,length_input:]
+
+    probs = gather_2d_on_last_dim(probs[valid_query_indices,:,:], output_ids, output_ids.shape)
+
+    clip_extra_output_ids = [(t == question_mark_ind).nonzero(as_tuple=True)[0][0] for t in output_ids] 
+
+    masks = torch.arange(probs.shape[1]).expand(len(clip_extra_output_ids), probs.shape[1]) < clip_extra_output_ids.unsqueeze(1)
+
+    probs = probs * masks
+
+    probs = torch.sum(axis=1)/torch.stack(clip_extra_output_ids)
+
+
+
+    final_probs = [0] * len(final_queries)
+
+    for idx,prob in zip(valid_query_indices,probs):
+        final_probs[idx] = prob
+
+    return final_queries, final_probs
+
+
+
+if __name__ == '__main__':
+    # use :
+    # python3 generate_queries_gptneo.py --original_doc /home/ubuntu/output.csv --aug_query aug_query.tsv --aug_query_qrels aug_query_qrels.txt
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--original_doc', type=str, required=True,
+                        help='Full Path to TSV file containing document IDs and texts')
+    parser.add_argument('--aug_query', type=str, required=True,
+                        help='Full Path of TSV file where generated query and their new IDs will be written')
+    parser.add_argument('--aug_query_qrels', type=str, required=True,
+                        help='Full Path of .txt file to store the query aand document ID relation pairs')
+    parser.add_argument('--engine', type=str, default="EleutherAI/gpt-neo-1.3B")
+    parser.add_argument('--prompt_template', type=str, default='prompts/vanilla_prompt.txt')
+    parser.add_argument('--max_examples', type=int, default=10,
+                        help='Maximum number of documents to read from the collection.')
+    parser.add_argument('--max_tokens', type=int, default=32, help='Max tokens to be generated.')
+    parser.add_argument('--batch_size', type=int, default=12, help="dataloader batch size")
+    parser.add_argument('--temperature', type=float, default=0.0,
+                        help='Sampling temperature. Zero means greedy decoding.')
+    parser.add_argument('--top_p', type=float, default=1.0)
+    parser.add_argument('--sleep_time', type=float, default=1.5, 
+                        help='Time to wait between API calls, in seconds.')        
+    parser.add_argument('--include_doc_probs', action='store_true',
+                        help='Wheter or not to save the tokens probabilities produeced by the model.')  
+
+    args = parser.parse_args()
+
+
+    query_id_timestamp = time.time()
+    query_id_counter = 0
+
+    inpars_dataset = InParsDataset(args.original_doc, args.prompt_template, args.max_examples)          
+            
+    tokenizer_class, model_class = ModelInitialization(args.engine).initialize_model()
+    tokenizer = tokenizer_class.from_pretrained(args.engine)
+    tokenizer.padding_side = "left" 
+    tokenizer.pad_token = tokenizer.eos_token
+
+    question_mark_id = tokenizer("?")["input_ids"][0]
+
+    model = model_class.from_pretrained(args.engine, return_dict_in_generate=True)
+    model = model.to(device)
+
+    inpars_collater = InParsCollater(tokenizer)
+    inpars_loader = DataLoader(inpars_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=inpars_collater)
+
+    start_time = time.time()
+    output_text = []
+    for i, batch in enumerate(inpars_loader):
+        
+        input_data = batch[1]['input_ids'].to(device=next(model.parameters()).device)
+        model_out = model.generate(input_data, do_sample=True, max_new_tokens=args.max_tokens,output_scores=True)
+        gen_text = tokenizer.batch_decode(model_out["sequences"])
+
+        final_queries, final_probs = postprocess_queries(gen_text, batch[2],model_out, question_mark_id,)
+        query_id_counter = write_to_output(final_queries,final_probs,batch[0], args.aug_query,args.aug_query_qrels, query_id_timestamp, query_id_counter)
+    print("Total Time = {0}".format(time.time()-start_time))
+
+
+    print('Done!')
