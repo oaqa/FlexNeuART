f8f4153561d3bd83d3dccfdd14977cbf43cb6b0a
Vivek Sourabh <viveksourabh2808@gmail.com>
2022-10-31 21:18:03 -0400

    implement name property

--- flexneuart/data_augmentation/__init__.py
+++ flexneuart/data_augmentation/__init__.py
@@ -1,18 +1,19 @@
 import os
 import importlib
 
-from flexneuart.data_augmentation.utils.data_augment import DataAugmentTest
+from flexneuart.data_augmentation.rule_based.data_augment import DataAugment
 
 # decorator implementation inspired from Fairseq
 
 AUGMENTATION_REGISTRY = {}
 AUGMENTATION_CLASS_NAMES = set()
+CLASS_TO_NAME = {}
 
 def register_augmentation(name):
     def register_task_cls(cls):
         if name in AUGMENTATION_REGISTRY:
             raise ValueError("Cannot register duplicate task ({})".format(name))
-        if not issubclass(cls, DataAugmentTest):
+        if not issubclass(cls, DataAugment):
             raise ValueError(
                 "Augmentation Method ({}: {}) must extend DataAugment".format(name, cls.__name__)
             )
@@ -24,13 +25,17 @@ def register_augmentation(name):
             )
         AUGMENTATION_REGISTRY[name] = cls
         AUGMENTATION_CLASS_NAMES.add(cls.__name__)
+        CLASS_TO_NAME[cls] = name
 
         return cls
 
     return register_task_cls
 
-def get_augmentation_method(name):
-    return AUGMENTATION_REGISTRY[name]
+def get_augmentation_method(name, conf):
+    return AUGMENTATION_REGISTRY[name](name, conf)
+
+def get_registered_name(cls):
+    return CLASS_TO_NAME[cls]
 
 def import_augment_methods(mod_dir, namespace):
     for file in os.listdir(mod_dir):
@@ -46,4 +51,4 @@ def import_augment_methods(mod_dir, namespace):
             importlib.import_module(namespace + "." + aug_name)
 
 mod_dir = os.path.dirname(__file__)
-import_augment_methods(os.path.join(mod_dir, "utils"), "flexneuart.data_augmentation.utils")
+import_augment_methods(os.path.join(mod_dir, "rule_based"), "flexneuart.data_augmentation.rule_based")
