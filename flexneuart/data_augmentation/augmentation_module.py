565f4164c108dab349468716a23efdf1467d52eb
Preksha Patel <prekshaupatel@gmail.com>
2022-11-12 18:09:29 -0500

    changes

--- flexneuart/data_augmentation/augmentation_module.py
+++ flexneuart/data_augmentation/augmentation_module.py
@@ -1,20 +1,26 @@
 import random
 from flexneuart.data_augmentation import get_augmentation_method
+from flexneuart.data_augmentation.rule_based.utils.parameters import conf as default_conf
 import json
 
 class DataAugmentModule:
-    def __init__(self, augment_type, random_seed=42, augment_p=0.25):
+    def __init__(self, augment_type, config_path, augment_p=0.25):
         self.p = augment_p
         random.seed(random_seed)
         self.doc_augment = None
-        config_path = "/home/ubuntu/efs/capstone/data_aug/FlexNeuART/flexneuart/data_augmentation/rule_based/parameters.json"
-        conf_file = open(config_path)
-        conf = json.load(conf_file)
-        conf_file.close()        
+        if config_path == "":
+            conf = default_conf
+        else:
+            try:
+                conf_file = open(config_path)
+                conf = json.load(conf_file)
+                conf_file.close()
+            except:
+                raise Exception("Please ensure that the path to the config file is correct.")
         try:
             self.doc_augment = get_augmentation_method(augment_type, conf)
         except:
-            print("No Augmentation Registered Augmentation Found by the name {0}".format(augment_type))
+            raise Exception("No Augmentation Registered Augmentation Found by the name {0}".format(augment_type))
             pass
 
     def augment(self, query_text, doc_text):
