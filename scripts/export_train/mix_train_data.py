d0bc40340da920d630bd1bea6db9817b17386537
searchivarius <leo@boytsov.info>
2023-04-06 04:28:32 -0400

    mixing script refactoring.

--- scripts/export_train/mix_train_data.py
+++ scripts/export_train/mix_train_data.py
@@ -36,118 +36,90 @@ from flexneuart.io.runs import read_run_dict, write_run_dict
 from flexneuart.io.qrels import read_qrels_dict
 from flexneuart.utils import set_all_seeds
 
-
 def prefix_key(d, pref):
     return {pref + k : v for k, v in d.items()}
-
 
 def prefix_key_val(d, pref):
     return {pref + k : prefix_key(v, pref) for k, v in d.items()}
 
+parser = argparse.ArgumentParser('Mix multiple training sets in CEDR format (evaluation file is taken from the last one)')
 
-
-parser = argparse.ArgumentParser('Mix two training sets in CEDR format')
-
-parser.add_argument('--dir1', type=str, required=True)
-parser.add_argument('--out_pref1', type=str, default='c1_')
-
-parser.add_argument('--dir2', type=str, required=True)
-parser.add_argument('--out_pref2', type=str, default='c2_')
-
-parser.add_argument('--dir_out', type=str, required=True)
-
+parser.add_argument('--input_dirs', type=str, nargs='+', required=True)
+parser.add_argument('--output_dir', type=str, required=True)
 parser.add_argument('--seed', type=int, default=0)
-
-parser.add_argument('--prob1', type=float, default=1)
-parser.add_argument('--prob2', type=float, default=1)
+parser.add_argument('--prob', type=float, nargs='+')
 
 args = parser.parse_args()
 print(args)
 
 set_all_seeds(args.seed)
 
-assert args.prob1 > 0 and args.prob1 <= 1
-assert args.prob2 > 0 and args.prob2 <= 1
+input_dirs = args.input_dirs
+output_dir = args.output_dir
+input_qty = len(input_dirs)
+assert input_qty
 
-os.makedirs(args.dir_out, exist_ok=True)
+if args.prob:
+    probs = np.array(args.prob)
+    assert np.all(probs > 0), 'probabilities should be > 0'
+    assert np.all(probs <= 1), 'probabilities should be <= 1'
+else:
+    probs = np.full(1, input_qty)
 
-# Read convert & save the run
-run1 = prefix_key_val(read_run_dict(os.path.join(args.dir1, TEST_RUN)), args.out_pref1)
-write_run_dict(run1, os.path.join(args.dir_out, TEST_RUN))
+assert len(input_dirs) == len(probs) or len(probs) == 0, \
+    'The number of probabilities should match the number of input directories (or do not use probabilities at all)'
 
-queries1, data1 = read_datafiles([os.path.join(args.dir1, fn) for fn in [DATA_DOCS, DATA_QUERY]])
+print('Mixing probabilities:', probs)
 
-train_pairs1 = prefix_key_val(read_pairs_dict(os.path.join(args.dir1, TRAIN_PAIRS)), args.out_pref1)
+os.makedirs(output_dir, exist_ok=True)
 
-queries1 = prefix_key(queries1, args.out_pref1)
-qrels1 = prefix_key_val(read_qrels_dict(os.path.join(args.dir1, QRELS)), args.out_pref1)
-data1 = prefix_key(data1, args.out_pref1)
+with open_with_default_enc(os.path.join(output_dir, QRELS), 'w') as out_qrels_f:
+    with open_with_default_enc(os.path.join(output_dir, DATA_DOCS), 'w') as out_data_f:
+        with open_with_default_enc(os.path.join(output_dir, DATA_QUERY), 'w') as out_query_f:
+            with open_with_default_enc(os.path.join(output_dir, TRAIN_PAIRS), 'w') as out_trainp_f:
+                for inp_id, inp_dir in enumerate(input_dirs):
+                    out_pref = f'set_{inp_id}'
 
-queries2, data2 = read_datafiles([os.path.join(args.dir2, fn) for fn in [DATA_DOCS, DATA_QUERY]])
+                    dids = []
+                    qidx = []
 
-queries2 = prefix_key(queries2, args.out_pref2)
-qrels2 = prefix_key_val(read_qrels_dict(os.path.join(args.dir2, QRELS)), args.out_pref2)
-data2 = prefix_key(data2, args.out_pref2)
+                    # Read convert & save the run
+                    if inp_id + 1 >= input_qty:
+                        run = prefix_key_val(read_run_dict(os.path.join(inp_dir, TEST_RUN)), out_pref)
+                        write_run_dict(run, os.path.join(output_dir, TEST_RUN))
 
-train_pairs2 = prefix_key_val(read_pairs_dict(os.path.join(args.dir2, TRAIN_PAIRS)), args.out_pref2)
+                        # We need to add validation query/document IDs to the set of queries and documents to save.
+                        for qid, did_dict in run.items():
+                            qids.add(qid)
+                            dids.extend(list(did_dict.keys()))
 
-qids1 = list(train_pairs1.keys())
-if args.prob1 < 1:
-    qids1 = list(np.random.choice(qids1, size=int(args.prob1 * len(qids1)), replace=False))
+                    queries, data = read_datafiles([os.path.join(inp_dir, fn) for fn in [DATA_DOCS, DATA_QUERY]])
+                    queries = prefix_key(queries, out_pref)
+                    data = prefix_key(data, out_pref)
 
-print('Selected ', len(qids1), 'queries from ', len(train_pairs1))
+                    train_pairs = prefix_key_val(read_pairs_dict(os.path.join(inp_dir, TRAIN_PAIRS)), out_pref)
+                    qids.extend(list(train_pairs.keys()))
 
-qids2 = list(train_pairs2.keys())
-if args.prob2 < 2:
-    qids2 = list(np.random.choice(qids2, size=int(args.prob2 * len(qids2)), replace=False))
+                    qrels = prefix_key_val(read_qrels_dict(os.path.join(inp_dir, QRELS)), out_pref)
 
-print('Selected ', len(qids2), 'queries from ', len(train_pairs2))
+                    prob = probs[inp_id]
+                    if prob < 1:
+                        qids = list(np.random.choice(qids, size=int(prob * len(qids)), replace=False))
 
-qids_all = set(qids1 + qids2)
+                    print('Set', inp_id, 'Selected ', len(qids), 'training queries from ', len(train_pairs))
 
-dids_all1 = []
-for qid, did_dict in train_pairs1.items():
-    if qid in qids_all:
-        dids_all1.extend(list(did_dict.keys()))
-dids_all1 = set(dids_all1)
-print('Selected', len(dids_all1), 'documents from ', len(data1))
+                    qids = set(qids)
 
-dids_all2 = []
-for qid, did_dict in train_pairs2.items():
-    if qid in qids_all:
-        dids_all2.extend(list(did_dict.keys()))
-dids_all2 = set(dids_all2)
-print('Selected', len(dids_all2), 'documents from ', len(data2))
+                    for qid, did_dict in train_pairs.items():
+                        if qid in qids:
+                            dids.extend(list(did_dict.keys()))
+                    dids = set(dids)
+                    print('Selected', len(dids, 'documents from ', len(data)))
 
-dids_all = set(list(dids_all1) + list(dids_all2))
-
-#
-# Finally, we need to add validation query/document IDs to the set of
-# queries and documents to save. Otherwise,  respective queries and/or
-# documents may be missing.
-#
-
-for qid, did_dict in run1.items():
-    qids_all.add(qid)
-    for did in did_dict.keys():
-        dids_all.add(did)
-
-with open_with_default_enc(os.path.join(args.dir_out, QRELS), 'w') as out_qrels_f:
-    write_filtered_qrels(out_qrels_f, qrels1, qids_all)
-    write_filtered_qrels(out_qrels_f, qrels2, qids_all)
-
-with open_with_default_enc(os.path.join(args.dir_out, DATA_DOCS), 'w') as out_data_f:
-    write_filtered_datafiles(out_data_f, data1, DATA_TYPE_DOC, dids_all)
-    write_filtered_datafiles(out_data_f, data2, DATA_TYPE_DOC, dids_all)
-
-with open_with_default_enc(os.path.join(args.dir_out, DATA_QUERY), 'w') as out_query_f:
-    write_filtered_datafiles(out_query_f, queries1, DATA_TYPE_QUERY, qids_all)
-    write_filtered_datafiles(out_query_f, queries2, DATA_TYPE_QUERY, qids_all)
-
-
-write_filtered_train_pairs(os.path.join(args.dir_out, TRAIN_PAIRS),
-                           merge_dict(train_pairs1, train_pairs2),
-                           qids_all)
+                    write_filtered_qrels(out_qrels_f, qrels, qids)
+                    write_filtered_datafiles(out_data_f, data, DATA_TYPE_DOC, dids)
+                    write_filtered_datafiles(out_query_f, queries, DATA_TYPE_QUERY, qids)
+                    write_filtered_train_pairs(out_trainp_f, train_pairs, qids)
 
 
 
